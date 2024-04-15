61ad184ef71177f76dd8356d54126a53f3f5d8fd
M B <mirrir@mirrir.net>
2024-04-15 11:25:49 -0600

    fix type checking

--- backend/models/utils.py
+++ backend/models/utils.py
@@ -84,4 +84,5 @@ class RepoMixin:
 
 class PydanticBaseModel(_BaseModel):
     """Extend Pydantic's BaseModel to enable ORM mode"""
-    model_config = {"from_attributes": True}
+
+    model_config = {"from_attributes": True, "arbitrary_types_allowed": True}
