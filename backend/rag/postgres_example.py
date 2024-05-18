7c45703687aff749ab56dff0fa07a71f080a3201
Nicole Venner <nvenner@protonmail.ch>
2024-05-18 09:03:13 -0600

    fix url for db connection

--- backend/rag/postgres_example.py
+++ backend/rag/postgres_example.py
@@ -72,7 +72,7 @@ Using an existing postgres running at localhost, create the database we'll be us
 
 import psycopg2
 
-connection_string = "postgresql://postgres:password@db:5432"
+connection_string = "postgresql://postgres:password@database:5432"
 db_name = "vector_db"
 conn = psycopg2.connect(connection_string)
 conn.autocommit = True
