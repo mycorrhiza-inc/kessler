bbd3b918c1d4bb12a90dc872a8ce33db67b54249
Nicole Venner <nvenner@protonmail.ch>
2024-05-19 07:30:08 -0600

    fix async postgres problem

--- backend/models/utils.py
+++ backend/models/utils.py
@@ -19,6 +19,8 @@ from pydantic import BaseModel as _BaseModel
 session_config = AsyncSessionConfig(expire_on_commit=False)
 
 postgres_connection_string = os.environ["DATABASE_CONNECTION_STRING"]
+if "postgresql://" in postgres_connection_string:
+    postgres_connection_string = postgres_connection_string.replace("postgresql://", "postgresql+asyncpg://")
 
 sqlalchemy_config = SQLAlchemyAsyncConfig(
     connection_string=postgres_connection_string,
