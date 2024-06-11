fc72ae25153933435aba514f52d04802ffbd3e01
Nicole Venner <nvenner@protonmail.ch>
2024-06-11 13:31:12 -0600

    fix type issues

--- frontend/src/lib/components/MarkdownRenderer.tsx
+++ frontend/src/lib/components/MarkdownRenderer.tsx
@@ -4,9 +4,13 @@ import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 import remarkGfm from 'remark-gfm'
 
-const MarkdownRenderer = ({ children }) => {
+interface MarkdownRendererProps {
+  children: string;
+}
+
+const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ children }) => {
   return (
-    <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+    <Markdown remarkPlugins={[remarkMath, remarkGfm]} rehypePlugins={[rehypeKatex]}>
       {children}
     </Markdown>
   );
