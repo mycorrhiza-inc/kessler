d72621611c23067739a4f8628d3708c5c0ccb5ba
Nicole Venner <nvenner@protonmail.ch>
2024-06-11 13:34:10 -0600

    add syntax highlight chunk

--- frontend/src/lib/components/MarkdownRenderer.tsx
+++ frontend/src/lib/components/MarkdownRenderer.tsx
@@ -3,6 +3,10 @@ import Markdown from 'react-markdown';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
 
 interface MarkdownRendererProps {
   children: string;
@@ -10,7 +14,25 @@ interface MarkdownRendererProps {
 
 const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ children }) => {
   return (
-    <Markdown remarkPlugins={[remarkMath, remarkGfm]} rehypePlugins={[rehypeKatex]}>
+    <Markdown
+      remarkPlugins={[remarkMath, remarkGfm]}
+      rehypePlugins={[rehypeKatex, rehypeRaw]}
+      components={{
+        code({ node, inline, className, children, ...props }: any) {
+          const match = /language-(\w+)/.exec(className || '');
+
+          return !inline && match ? (
+            <SyntaxHighlighter style={dracula} PreTag="div" language={match[1]} {...props}>
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
       {children}
     </Markdown>
   );
