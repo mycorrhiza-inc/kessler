efe19c48ddc6b64efff395e85751ed0599b61db1
M B <mirrir@mirrir.net>
2024-06-13 18:00:08 -0600

    remove BaseLance and use chunk_ids to map between files and their chunks

--- backend/models/chunks.py
+++ backend/models/chunks.py
@@ -1,10 +1,21 @@
 from lance_store.tables import BaseLance
+from lancedb.pydantic import LanceModel, Vector
 from lance_store.embeddings import func
+
+from datetime import datetime
+
 from typing import Optional
 
 
-class Chunks(BaseLance):
+class Chunks(LanceModel):
     # every single chunk
-    __tablename__ = "text_chunks"
+    vector: Vector(func.ndims()) = func.VectorField()
     text: str = func.SourceField()
-    location: Optional[tuple[int, int] | None] = None  # row, col
+    created: datetime
+    modified: datetime
+    published: Optional[datetime | None]
+    # since its a UUID the chance of collision is minimal
+    parent_id: str
+    chunk_id: str
+    # row, col of the parent
+    location: Optional[tuple[int, int] | None] = None
