3dcf70ccb08f288fa68f1a635c6965c3e888537a
Nicole Venner <nvenner@protonmail.ch>
2024-06-29 20:13:30 -0600

    attempt at basic hydration

--- frontend/src/app/_app.tsx
+++ frontend/src/app/_app.tsx
@@ -1,5 +1,6 @@
 // src/pages/_app.tsx
 
+import { Chakra } from "../lib/Chakra";
 import { AppProps } from "next/app";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "../app/theme";
@@ -7,11 +8,14 @@ import "../app/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Chakra cookies={pageProps.cookies}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Chakra>
   );
 }
 
 export default MyApp;
+export { getServerSideProps } from "../lib/Chakra";
