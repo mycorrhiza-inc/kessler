c07baae91f583dafda624975f2fb5864292634be
Nicole Venner <nvenner@protonmail.ch>
2024-07-01 23:34:12 -0600

    figured out that none of the SSR code was actually fucking running

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,11 @@
-import type { Metadata } from "next";
+// layout.tsx
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { SaasProvider } from "@saas-ui/react";
+import { ChakraProvider, ColorModeScript, DarkMode } from "@chakra-ui/react";
+import theme from "../app/theme";
+import "../app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +23,43 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
-          <SaasProvider>{children}</SaasProvider>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <ChakraProvider theme={theme}>
+            <DarkMode>
+              <SaasProvider>{children}</SaasProvider>
+            </DarkMode>
+          </ChakraProvider>
         </body>
       </html>
     </ClerkProvider>
   );
 }
+
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import { ClerkProvider } from "@clerk/nextjs";
+// import { SaasProvider } from "@saas-ui/react";
+//
+// const inter = Inter({ subsets: ["latin"] });
+//
+// export const metadata: Metadata = {
+//   title: "Kessler Search",
+//   description: "the kessler search engine",
+// };
+//
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <ClerkProvider>
+//       <html lang="en">
+//         <body>
+//           <SaasProvider>{children}</SaasProvider>
+//         </body>
+//       </html>
+//     </ClerkProvider>
+//   );
+// }
