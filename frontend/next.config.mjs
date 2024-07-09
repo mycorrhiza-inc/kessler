64bfbe6d6995e8851587335f89d615589c437cc2
Nicole Venner <nvenner@protonmail.ch>
2024-07-09 08:43:48 -0600

    potential fix for redirect bug

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@clerk/clerk-react", "@saas-ui/clerk"],
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
+  },
 };
 
 export default nextConfig;
