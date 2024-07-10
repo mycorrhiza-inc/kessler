9ffadc9ad325e15f324d1f0529b8cb6635aa4111
M Bright <mbright@mycor.io>
2024-07-10 16:38:23 -0600

    Jul10-1638 checkpoint

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,59 +1,108 @@
 'use client';
 
-import { Container, Box, Input, Flex } from '@chakra-ui/react';
+import { Container, Box, Input, Flex, CircularProgress, Spacer, Center } from '@chakra-ui/react';
 import { ChangeEvent, useEffect, useState } from 'react';
 
 
 
-
-const ResultComponent = ({ content }: { content: string }) => {
+const ResultComponent = ({ item, index }: { item: any, index: number }) => {
   return <Container className='searchResult'>
+    <div key={index} className="data-item">
+      <p>ID: {item.id}</p>
+      <p>Score: {item.score}</p>
+      <p>Author: {item.metadata.author}</p>
+      <p>Title: {item.metadata.title}</p>
+    </div>
   </Container>
 }
-const SearchResultList = ({ ids }: { ids: string[] }) => {
-  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => setCount((count + 1) % 10), 1000);
-    // return () => clearInterval(interval);
-  }, []);
-
-  if (ids.length <= 0) {
-    return <>Loading {'.'.repeat(count)}</>
-  } else {
-    return <>
-      {ids.map((result) => (
-        <ResultComponent content={result} />
+const SearchResultList = ({ results, searching }: { results: any[], searching: boolean }) => {
+  return <>
+    {searching && (
+      <CircularProgress isIndeterminate color="green.300" />
+    )}
+    {results.length > 0 &&
+      results.map((item: any, index) => (
+        <>
+          <ResultComponent item={item} index={index} />
+        </>
       ))}
-    </>
-  }
+
+  </>
 }
 
-
-
 export default function SearchPage() {
-  const [queryText, setQuery] = useState('');
-  const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [searching, setSearching] = useState(false);
+  const [searchQuery, setQuery] = useState("");
+  const [searchResults, setResults] = useState([]);
+  const handleQueryChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    setResults([]);
+    setQuery(event.target.value);
+    setSearching(true);
+    await getSearchResults();
+  };
 
-  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => setQuery(event.target.value)
+  // search stuff
+  const getSearchResults = async () => {
+    let results = await fetch("/api/search", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: searchQuery }),
+    })
+      .then(async (response) => {
+        if (!response.ok) {
+          return response.json().then((err) => {
+            throw new Error(err.message);
+          });
+        }
+        return await response.json();
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        return data;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        return Symbol("error");
+      });
 
-  const evaluateSearch = () => {
-
-  }
+    setSearching(false);
+    if (results == Symbol("Error")) {
+      console.log("no search results");
+      return;
+    }
+    setResults(results);
+  };
 
   return <Box w='100vw' h='100vh' m='0' p='0'>
-    <Flex direction="column" minWidth='m' alignItems='center'>
-      <Container
+    <Flex direction="column" alignItems='center'>
+      <Center>
+        <TextArt label="Kessler" text="kess_logo" />
+      </Center>
+      <Spacer />
+      <Center
         className='searchInput'
         minW='90vw'
         justifySelf='center'
+        justifyContent='center'
       >
         <Input
-          value={queryText}
-          onChange={handleSearchChange}
+          maxW="50vw"
+          minW="500px"
+          value={searchQuery}
+          onChange={handleQueryChange}
         />
-      </Container>
-      <SearchResultList ids={searchResults} />
+      </Center>
+      <Spacer />
+      <Center>
+        <SearchResultList results={searchResults} searching={searching} />
+      </Center>
     </Flex>
   </Box>
 }
+
+
+
+
