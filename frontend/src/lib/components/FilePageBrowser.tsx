89cd243d975da867c3475da40da6128665854f7c
Nicole Venner <nvenner@protonmail.ch>
2024-07-18 08:59:39 -0600

    refactor filepagebrowser and add pagination

--- frontend/src/lib/components/FilePageBrowser.tsx
+++ frontend/src/lib/components/FilePageBrowser.tsx
@@ -1,16 +1,4 @@
-import {
-  Box,
-  Button,
-  Center,
-  Select,
-  Spinner,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
+import { Box, Button, Center, Select, Spinner } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import FileTable from "./FileTable";
 import { FileType } from "../interfaces/file";
@@ -48,7 +36,7 @@ const FilePageBrowser: React.FC<FilePageBrowserProps> = ({ fileUrl, data }) => {
 
   useEffect(() => {
     fetchFiles();
-  }, [page, numResults]);
+  }, [fileUrl, data, page, numResults]);
 
   return (
     <Box>
@@ -60,25 +48,131 @@ const FilePageBrowser: React.FC<FilePageBrowserProps> = ({ fileUrl, data }) => {
         <FileTable files={files} />
       )}
       <Box mt={4} display="flex" justifyContent="space-between">
-        <Button
-          onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
-          isDisabled={page === 1}
-        >
-          Previous
-        </Button>
-        <Select
-          value={numResults}
-          onChange={(e) => {
-            setNumResults(Number(e.target.value));
-            setPage(1);
-          }}
-        >
-          <option value={10}>10</option>
-          <option value={20}>20</option>
-          <option value={50}>50</option>
-        </Select>
-        <Button onClick={() => setPage((prev) => prev + 1)}>Next</Button>
+        <Paginator page={page} setPage={setPage} maxPage={maxPage} />
+        <br />
+        <NumResultsSelector
+          numResults={numResults}
+          setNumResults={setNumResults}
+          setPage={setPage}
+        />
       </Box>
+    </Box>
+  );
+};
+
+interface NumResultsSelectorProps {
+  numResults: number;
+  setNumResults: (num: number) => void;
+  setPage: (page: number) => void;
+}
+
+const NumResultsSelector: React.FC<NumResultsSelectorProps> = ({
+  numResults,
+  setNumResults,
+  setPage,
+}) => {
+  return (
+    <Select
+      value={numResults}
+      onChange={(e) => {
+        setNumResults(Number(e.target.value));
+        setPage(1);
+      }}
+    >
+      <option value={10}>10</option>
+      <option value={25}>25</option>
+      <option value={50}>50</option>
+      <option value={100}>100</option>
+      <option value={500}>500</option>
+      <option value={65535}>Unlimited</option>
+    </Select>
+  );
+};
+
+interface PaginatorProps {
+  page: number;
+  setPage: (page: number) => void;
+  maxPage: number;
+}
+
+const Paginator: React.FC<PaginatorProps> = ({ page, setPage, maxPage }) => {
+  const createPageButtons = () => {
+    const buttons = [];
+    if (maxPage <= 6) {
+      for (let i = 1; i <= maxPage; i++) {
+        buttons.push(
+          <Button
+            key={i}
+            onClick={() => setPage(i)}
+            bg={i === page ? "blue.500" : "gray.200"}
+            color={i === page ? "white" : "black"}
+          >
+            {i}
+          </Button>,
+        );
+      }
+    } else {
+      buttons.push(
+        <Button
+          key={1}
+          onClick={() => setPage(1)}
+          bg={1 === page ? "blue.500" : "gray.200"}
+          color={1 === page ? "white" : "black"}
+        >
+          1
+        </Button>,
+      );
+      if (page > 5) {
+        buttons.push(<span key="front-ellipsis">...</span>);
+      }
+      for (
+        let i = Math.max(2, page - 1);
+        i <= Math.min(maxPage - 1, page + 1);
+        i++
+      ) {
+        buttons.push(
+          <Button
+            key={i}
+            onClick={() => setPage(i)}
+            bg={i === page ? "blue.500" : "gray.200"}
+            color={i === page ? "white" : "black"}
+          >
+            {i}
+          </Button>,
+        );
+      }
+      if (maxPage - page > 5) {
+        buttons.push(<span key="back-ellipsis">...</span>);
+      }
+      buttons.push(
+        <Button
+          key={maxPage}
+          onClick={() => setPage(maxPage)}
+          bg={maxPage === page ? "blue.500" : "gray.200"}
+          color={maxPage === page ? "white" : "black"}
+        >
+          {maxPage}
+        </Button>,
+      );
+    }
+    return buttons;
+  };
+
+  return (
+    <Box display="flex" alignItems="center">
+      <Button
+        onClick={() => setPage(Math.max(page - 1, 1))}
+        isDisabled={page === 1}
+      >
+        Previous
+      </Button>
+      {createPageButtons()}
+      <Button
+        onClick={() => setPage(Math.min(page + 1, maxPage))}
+        isDisabled={page === maxPage}
+      >
+        Next
+      </Button>
     </Box>
   );
 };
