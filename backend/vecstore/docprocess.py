e2a7803d6a64a1ad1a5b3b40d12da1105be9674f
M Bright <mbright@mycor.io>
2024-07-27 14:11:56 -0600

    correct typing for add_doc_to_db

--- backend/vecstore/docprocess.py
+++ backend/vecstore/docprocess.py
@@ -2,6 +2,6 @@ from rag.SemanticSplitter import SemanticSplitter
 from .util import add_nodes
 
 
-def add_document_to_db(text: str, metadata: dict, collection_name: str):
+def add_document_to_db(text: str, metadata: dict[str, str], collection_name: str):
     nodes = SemanticSplitter().process(text)
     add_nodes(nodes, collection_name=collection_name, metadata=metadata)
