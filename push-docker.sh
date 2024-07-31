8f0f93059811493e0a4972436dbe64d2ad3dca95
M Bright <mbright@mycor.io>
2024-07-31 17:59:13 -0600

    amd64 build

--- push-docker.sh
+++ push-docker.sh
@@ -1,6 +1,6 @@
-#! /usr/bin/bash
-docker build -t fractalhuman1/kessler-frontend:latest ./frontend/
-docker build -t fractalhuman1/kessler-backend:latest ./backend/
+#! /bin/bash
+docker build -t fractalhuman1/kessler-frontend:latest --platform linux/amd64 ./frontend/
+docker build -t fractalhuman1/kessler-backend:latest --platform linux/amd64 ./backend/
 
 docker push fractalhuman1/kessler-frontend:latest
 docker push fractalhuman1/kessler-backend:latest
