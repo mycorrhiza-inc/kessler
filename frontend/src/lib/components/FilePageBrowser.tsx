345c02875d3f25c85bec112e1903f31c4bdb0edd
Nicole Venner <nvenner@protonmail.ch>
2024-08-15 10:01:23 -0600

    add initial nimbus integration

--- frontend/src/lib/components/FilePageBrowser.tsx
+++ frontend/src/lib/components/FilePageBrowser.tsx
@@ -5,6 +5,7 @@ import CustomizeFileTableButton from "./CustomizeFileTableButton";
 import { FileType } from "../interfaces/file";
 
 import { defaultLayout } from "./FileTable";
+import Paginator from "./Paginator";
 
 interface FilePageBrowserProps {
   fileUrl: string;
@@ -51,138 +52,13 @@ const FilePageBrowser: React.FC<FilePageBrowserProps> = ({ fileUrl, data }) => {
       ) : (
         <FileTable files={files} layout={layout} />
       )}
-      <Box mt={4} display="flex" justifyContent="space-between">
-        <Paginator page={page} setPage={setPage} maxPage={maxPage} />
-      </Box>
-      <Box>
-        <Text>
-          <br />
-          Select Number of Results
-        </Text>
-        <NumResultsSelector
-          numResults={numResults}
-          setNumResults={setNumResults}
-          setPage={setPage}
-        />
-      </Box>
-    </Box>
-  );
-};
-
-interface NumResultsSelectorProps {
-  numResults: number;
-  setNumResults: (num: number) => void;
-  setPage: (page: number) => void;
-}
-
-const NumResultsSelector: React.FC<NumResultsSelectorProps> = ({
-  numResults,
-  setNumResults,
-  setPage,
-}) => {
-  return (
-    <Select
-      value={numResults}
-      onChange={(e) => {
-        setNumResults(Number(e.target.value));
-        setPage(1);
-      }}
-    >
-      <option value={1}>1</option>
-      <option value={10}>10</option>
-      <option value={25}>25</option>
-      <option value={50}>50</option>
-      <option value={100}>100</option>
-      <option value={500}>500</option>
-      <option value={65535}>Unlimited</option>
-    </Select>
-  );
-};
-
-interface PaginatorProps {
-  page: number;
-  setPage: (page: number) => void;
-  maxPage: number;
-}
-
-const Paginator: React.FC<PaginatorProps> = ({ page, setPage, maxPage }) => {
-  const createPageButtons = () => {
-    const buttons = [];
-    if (maxPage <= 6) {
-      for (let i = 1; i <= maxPage; i++) {
-        buttons.push(
-          <Button
-            key={i}
-            onClick={() => setPage(i)}
-            bg={i === page ? "blue.500" : "gray.200"}
-            color={i === page ? "white" : "black"}
-          >
-            {i}
-          </Button>,
-        );
-      }
-    } else {
-      buttons.push(
-        <Button
-          key={1}
-          onClick={() => setPage(1)}
-          bg={1 === page ? "blue.500" : "gray.200"}
-          color={1 === page ? "white" : "black"}
-        >
-          1
-        </Button>,
-      );
-      if (page > 5) {
-        buttons.push(<span key="front-ellipsis">...</span>);
-      }
-      for (
-        let i = Math.max(2, page - 1);
-        i <= Math.min(maxPage - 1, page + 1);
-        i++
-      ) {
-        buttons.push(
-          <Button
-            key={i}
-            onClick={() => setPage(i)}
-            bg={i === page ? "blue.500" : "gray.200"}
-            color={i === page ? "white" : "black"}
-          >
-            {i}
-          </Button>,
-        );
-      }
-      if (maxPage - page > 5) {
-        buttons.push(<span key="back-ellipsis">...</span>);
-      }
-      buttons.push(
-        <Button
-          key={maxPage}
-          onClick={() => setPage(maxPage)}
-          bg={maxPage === page ? "blue.500" : "gray.200"}
-          color={maxPage === page ? "white" : "black"}
-        >
-          {maxPage}
-        </Button>,
-      );
-    }
-    return buttons;
-  };
-
-  return (
-    <Box display="flex" alignItems="center">
-      <Button
-        onClick={() => setPage(Math.max(page - 1, 1))}
-        isDisabled={page === 1}
-      >
-        Previous
-      </Button>
-      {createPageButtons()}
-      <Button
-        onClick={() => setPage(Math.min(page + 1, maxPage))}
-        isDisabled={page === maxPage}
-      >
-        Next
-      </Button>
+      <Paginator
+        page={page}
+        setPage={setPage}
+        maxPage={maxPage}
+        numResults={numResults}
+        setNumResults={setNumResults}
+      />
     </Box>
   );
 };
---
+++ frontend/src/lib/components/ModelUtils.tsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Grid,
+  GridItem,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import ReactDOM from "react-dom";
+import MarkdownRenderer from "./MarkdownRenderer";
+import PDFViewer from "./PDFViewer";
+import { FileType } from "../interfaces";
+
+import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+const DynamicModalIframe: React.FC<{
+  iframeURL: string;
+  onClose: () => void;
+}> = ({ iframeURL, onClose }) => {
+  const {
+    isOpen,
+    onOpen,
+    onClose: closeModal,
+  } = useDisclosure({ defaultIsOpen: true });
+
+  const toast = useToast();
+
+  useEffect(() => {
+    if (isOpen) {
+      (async () => {
+        // Do stuff when the modal opens
+      })();
+    }
+  }, [isOpen]);
+
+  const handleClose = () => {
+    closeModal();
+    onClose();
+  };
+
+  return ReactDOM.createPortal(
+    <Modal isOpen={isOpen} onClose={handleClose} size="6xl">
+      <ModalOverlay />
+      <ModalContent maxW="80%">
+        <ModalHeader></ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <iframe src={iframeURL} width="100%" height="100%" />
+        </ModalBody>
+        <ModalFooter>
+          <Button variant="ghost" onClick={handleClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>,
+    document.body,
+  );
+};
+
+const ViewIframeModal: React.FC<{ iframeURL: string; buttonName: string }> = ({
+  iframeURL,
+  buttonName,
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={handleOpenModal}>{buttonName}</Button>
+      {isModalOpen && (
+        <DynamicModalIframe iframeURL={iframeURL} onClose={handleCloseModal} />
+      )}
+    </>
+  );
+};
+
+// PDF Viewer Coming soon
+// <Document
+//   file="https://raw.githubusercontent.com/mycorrhizainc/examples/main/CO%20Clean%20Energy%20Plan%20Info%20Sheet.pdf"
+//   onLoadSuccess={onDocumentLoadSuccess}
+// >
+//   <Page pageNumber={pageNumber} />
+// </Document>
+export default ViewIframeModal;
