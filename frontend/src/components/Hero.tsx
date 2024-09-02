bc20ab649fa5296f7d47bf67440562d7c42467d2
Nicole Venner <nvenner@protonmail.ch>
2024-09-02 00:13:29 -0600

    more stuff

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,5 +1,5 @@
-import NextLogo from "./next-logo";
-import SupabaseLogo from "./supabase-logo";
+import NextLogo from "./supabasetutorial/next-logo";
+import SupabaseLogo from "./supabasetutorial/supabase-logo";
 
 export default function Header() {
   return (
