aaca45d549b9bbc5b82c8b47c2d2e9bd06bc5c3d
Nicole Venner <nvenner@protonmail.ch>
2024-09-10 00:57:36 -0600

    made landing page actually a landingpageish

--- frontend/src/app/start/page.tsx
+++ frontend/src/app/start/page.tsx
@@ -1,14 +1,10 @@
-import ConnectSupabaseSteps from "@/components/supabasetutorial/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/supabasetutorial/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import SearchApp from "@/components/SearchApp";
+import Landing from "@/components/landing/Landing";
 
-export default async function Index() {
+export default async function Page() {
   return (
-    <>
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
+    <div className="w-full">
+      <Landing></Landing>
+    </div>
   );
 }
---
+++ frontend/src/components/landing/Landing.tsx
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+
+export default function Landing() {
+  return (
+    <main className="flex min-w-screen h-100vh justify-center">
+      <div>
+        <Hero></Hero>
+      </div>
+    </main>
+  );
+}
