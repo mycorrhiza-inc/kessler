9d778a8ffc81e1a20f66dd90c11b00b5f7e3f257
Nicole Venner <nvenner@protonmail.ch>
2024-09-10 15:33:43 -0600

    added upsert file db logic

--- backend-python/models/files.py
+++ backend-python/models/files.py
@@ -24,6 +24,8 @@ from enum import Enum
 
 from sqlalchemy import select
 
+import asyncio
+
 
 class FileModel(UUIDAuditBase):
     """Database representation of a file"""
@@ -100,6 +102,95 @@ async def get_texts_from_file_uuid(
     ]
 
 
+async def delete_file_from_file_uuid(
+    async_db_connection: AsyncSession, file_id: UUID, recursive: bool = False
+):
+    await async_db_connection.execute(FileModel.delete().where(FileModel.id == file_id))
+    if recursive:
+        await async_db_connection.execute(
+            FileTextSource.delete().where(FileTextSource.file_id == file_id)
+        )
+
+
+async def upsert_file_from_full_schema(
+    async_db_connection: AsyncSession,
+    file: FileSchemaFull,
+    delete_old_recursive: bool = False,
+):
+    query = select(FileModel).where(FileModel.id == file.id)
+    result = await async_db_connection.execute(query)
+    existing_file = result.scalars().first()
+    file_exists = existing_file is not None
+
+    if delete_old_recursive and file_exists:
+        await delete_file_from_file_uuid(async_db_connection, file.id, True)
+
+    async def upsert_file_model(file: FileSchemaFull, file_exists: bool) -> None:
+        mdata_str = json.dumps(file.mdata)
+        values_dict = {
+            "id": file.id,
+            "url": file.url,
+            "doctype": file.doctype,
+            "lang": file.lang,
+            "name": file.name,
+            "source": file.source,
+            "hash": file.hash,
+            "mdata": mdata_str,
+            "stage": file.stage,
+            "summary": file.summary,
+            "short_summary": file.short_summary,
+        }
+        if file_exists:
+            update_stmt = (
+                FileModel.update().where(FileModel.id == file.id).values(**values_dict)
+            )
+            await async_db_connection.execute(update_stmt)
+        else:
+            insert_stmt = FileModel.insert().values(**values_dict)
+            await async_db_connection.execute(insert_stmt)
+
+    async def upsert_file_text_source(text_source: FileTextSchema) -> None:
+        await async_db_connection.execute(
+            select(FileTextSource).where(
+                FileTextSource.file_id == text_source.file_id,
+                FileTextSource.language == text_source.language,
+            )
+        )
+        file_text = result.scalars().first()
+        if file_text is None:
+            insert_stmt = (
+                FileTextSource.insert()
+                .values(
+                    id=UUID(),
+                    file_id=text_source.file_id,
+                    is_original_text=text_source.is_original_text,
+                    language=text_source.language,
+                    text=text_source.text,
+                )
+                .on_conflict_do_nothing()
+            )
+            await async_db_connection.execute(insert_stmt)
+        else:
+            update_stmt = (
+                FileTextSource.update()
+                .where(
+                    FileTextSource.id == text_source.id,
+                )
+                .values(
+                    text=text_source.text, is_original_text=text_source.is_original_text
+                )
+            )
+            await async_db_connection.execute(update_stmt)
+
+    if file.texts is None:
+        file.texts = []
+    async_tasks = [upsert_file_from_full_schema(async_db_connection, file)] + [
+        upsert_file_text_source(text) for text in file.texts
+    ]
+    await asyncio.gather(*async_tasks)
+    await async_db_connection.commit()
+
+
 # I wrote these 2 initially, but I think they might be redundant at this point.
 async def get_original_text_from_file_uuid(
     async_db_connection: AsyncSession, file_id: UUID
@@ -147,13 +238,6 @@ async def get_english_text_from_file_uuid(
             is_original_text=row.is_original_text,
         )
     return None
-
-
-async def upsert_file_from_full_schema(
-    async_db_connection: AsyncSession, file: FileSchemaFull
-):
-    mdata_str = json.dumps(file.mdata)
-    pass
 
 
 # Write a SQL Migration script that will take the english text of every document, and add it as a entry in the file text source table, with a unique UUID, the UUID of the original file as the file ID, a true value for the original text and "en" as the language?
