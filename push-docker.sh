62a4ba58a2e3dee63a9f0ba2362aa674f3bb1d47
Nicole Venner <nvenner@protonmail.ch>
2024-09-09 18:35:42 -0600

    maximum incompetence + more services working

--- push-docker.sh
+++ push-docker.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 docker build -t fractalhuman1/kessler-frontend:latest --platform linux/amd64 ./frontend/
-docker build -t fractalhuman1/kessler-backend:latest --platform linux/amd64 ./backend/
+docker build -t fractalhuman1/kessler-backend-python:latest --platform linux/amd64 ./backend-python/
+docker build -t fractalhuman1/kessler-backend-go:latest --platform linux/amd64 ./backend-go/
+# docker build -t fractalhuman1/kessler-thaumaturgy-python:latest --platform linux/amd64 ./thaumaturgy-python/
 
 docker push fractalhuman1/kessler-frontend:latest
-docker push fractalhuman1/kessler-backend:latest
+docker push fractalhuman1/kessler-backend-python:latest
+docker push fractalhuman1/kessler-backend-go:latest 
+# docker push fractalhuman1/kessler-thaumaturgy-python:latest
