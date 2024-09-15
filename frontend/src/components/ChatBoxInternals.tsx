a6600d00bd428d705aeabfc2a8517f274f1b0aba
Nicole Venner <nvenner@protonmail.ch>
2024-09-15 16:45:49 -0600

    commented out header, not info dense enough

--- frontend/src/components/ChatBoxInternals.tsx
+++ frontend/src/components/ChatBoxInternals.tsx
@@ -12,41 +12,7 @@ const ChatBoxInternals = ({
 }) => {
   return (
     <>
-      <div
-        className="chatbox-banner"
-        style={{
-          position: "sticky",
-          top: "0",
-          backgroundColor: "#f1f1f1",
-          padding: "20px",
-          textAlign: "center",
-          zIndex: "1000",
-          borderBottom: "1px solid #ccc",
-          height: "auto",
-          pointerEvents: "auto",
-        }}
-      >
-        <Stack direction="row" justifyContent="space-between">
-          <button
-            onClick={() => setChatSidebarVisible((prevState) => !prevState)}
-          ></button>
-        </Stack>
-      </div>
-      <div className="chatbox-banner sticky top-0 bg-[#f5f5f5] dark:bg-gray-700 p-5 text-center z-50 border-b border-gray-300 h-auto">
-        <div className="flex flex-row justify-between">
-          <button>
-            <HamburgerIcon />
-          </button>
-          <button
-            onClick={() => {
-              setChatVisible((prev) => !prev);
-            }}
-          >
-            <CloseIcon />
-          </button>
-        </div>
-      </div>
-      <div>
+      <div className="overflow-y-auto">
         <ChatMessages
           messages={exampleChatHistory}
           loading={false}
@@ -79,3 +45,39 @@ const ChatBoxInternals = ({
   );
 };
 export default ChatBoxInternals;
+// <div
+//   className="chatbox-banner"
+//   style={{
+//     position: "sticky",
+//     top: "0",
+//     backgroundColor: "#f1f1f1",
+//     padding: "20px",
+//     textAlign: "center",
+//     zIndex: "1000",
+//     borderBottom: "1px solid #ccc",
+//     height: "auto",
+//     pointerEvents: "auto",
+//   }}
+// >
+//   <Stack direction="row" justifyContent="space-between">
+//     <button
+//       onClick={() => setChatSidebarVisible((prevState) => !prevState)}
+//     ></button>
+//   </Stack>
+// </div>
+// <div className="chatbox-banner sticky top-0 bg-[#f5f5f5] dark:bg-gray-700 p-5 text-center z-50 border-b border-gray-300 h-auto">
+//   <div className="flex flex-row justify-between">
+//     <button>
+//       <HamburgerIcon />
+//     </button>
+//     <button
+//       onClick={() => {
+//         // This is fixable, dont want to worry about it now, if encounter bugs remove and actually solve.
+//         // @ts-ignore
+//         setChatVisible((prev) => !prev);
+//       }}
+//     >
+//       <CloseIcon />
+//     </button>
+//   </div>
+// </div>
