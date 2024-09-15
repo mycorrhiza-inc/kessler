8f3a9b62d6da882a411ccb6d6efceb0a6fdb3d16
Nicole Venner <nvenner@protonmail.ch>
2024-09-15 12:39:23 -0600

    fix more stuff

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,23 +16,31 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // const { theme } = useTheme();
+  // console.log(theme);
+  // const bg_color_tw = theme === "light" ? "bg-white" : "bg-black";
   return (
-    <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+    <html
+      lang="en"
+      className={GeistSans.className}
+      style={{ backgroundColor: "var(--background)" }}
+      suppressHydrationWarning
+    >
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <body className="bg-background text-foreground">
           <Header></Header>
           <main className="flex flex-col items-center">
             <div className="flex-1 w-100vw flex flex-col items-center">
               <div className="flex flex-col">{children}</div>
             </div>
           </main>
-        </ThemeProvider>
-      </body>
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
