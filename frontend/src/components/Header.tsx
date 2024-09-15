4456bc969e4de3dce84db1909283939ac27d2515
Nicole Venner <nvenner@protonmail.ch>
2024-09-15 16:36:17 -0600

    more color cleanup

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -30,7 +30,7 @@ async function HeaderAuth() {
 }
 const Header = () => {
   return (
-    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 bg-base-100">
+    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 bg-base-100 text-base-content">
       <div
         className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm"
         style={{ zIndex: 3000 }}
