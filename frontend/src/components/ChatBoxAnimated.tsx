c329972b51517cf566fb25da9904b647d4d6fe1c
Nicole Venner <nvenner@protonmail.ch>
2024-09-15 16:38:54 -0600

    dryify internals

--- frontend/src/components/ChatBoxAnimated.tsx
+++ frontend/src/components/ChatBoxAnimated.tsx
@@ -5,6 +5,7 @@ import { Dispatch, SetStateAction, useState } from "react";
 import { CloseIcon, HamburgerIcon } from "@/components/Icons";
 import "./ChatBoxAnimated.css";
 import { ChatMessages, exampleChatHistory } from "./ChatHistory";
+import ChatBoxInternals from "./ChatBoxInternals";
 
 interface ChatBoxProps {
   chatVisible: boolean;
@@ -121,73 +122,10 @@ const ChatBox = ({ chatVisible, setChatVisible, parentRef }: ChatBoxProps) => {
       {resizeHandle("bottom")}
       {resizeHandle("left")}
       {resizeHandle("right")}
-
-      <div
-        className="chatbox-banner"
-        style={{
-          position: "sticky",
-          top: "0",
-          backgroundColor: "#f1f1f1",
-          padding: "20px",
-          textAlign: "center",
-          zIndex: "1000",
-          borderBottom: "1px solid #ccc",
-          height: "auto",
-          pointerEvents: "auto",
-        }}
-      >
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          onPointerDown={startDrag}
-        >
-          <button
-            onClick={() => setChatSidebarVisible((prevState) => !prevState)}
-          ></button>
-        </Stack>
-      </div>
-      <div className="chatbox-banner sticky top-0 bg-[#f5f5f5] dark:bg-gray-700 p-5 text-center z-50 border-b border-gray-300 h-auto">
-        <div className="flex flex-row justify-between">
-          <button>
-            <HamburgerIcon />
-          </button>
-          <button
-            onClick={() => {
-              setChatVisible((prev) => !prev);
-            }}
-          >
-            <CloseIcon />
-          </button>
-        </div>
-      </div>
-      <div>
-        <ChatMessages
-          messages={exampleChatHistory}
-          loading={false}
-        ></ChatMessages>
-      </div>
-
-      <div
-        className="chatContainer"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          height: "90%",
-          width: "100%",
-          padding: "2px",
-        }}
-      >
-        <motion.div
-          className="chatSidebar"
-          animate={{
-            width: chatSidebarVisible ? "20%" : "0%",
-          }}
-          style={{ backgroundColor: "red", overflow: "scroll" }}
-        >
-          sidebar contents
-        </motion.div>
-        <div> chat contents</div>
-      </div>
+      <ChatBoxInternals
+        chatSidebarVisible={chatSidebarVisible}
+        setChatSidebarVisible={setChatSidebarVisible}
+      ></ChatBoxInternals>
     </motion.div>
   );
 };
