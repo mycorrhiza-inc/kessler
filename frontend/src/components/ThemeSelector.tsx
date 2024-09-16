0bb8fcc5293fbcbb205625834bf946fbbed63cc1
Nicole Venner <nvenner@protonmail.ch>
2024-09-16 13:54:19 -0600

    final commit on theme stuff

--- frontend/src/components/ThemeSelector.tsx
+++ frontend/src/components/ThemeSelector.tsx
@@ -11,8 +11,10 @@ const ThemeSelector = () => {
     "aqua",
   ];
   return (
-    <details className="dropdown">
-      <summary className="btn m-1">Theme</summary>
+    <div className="dropdown dropdown-hover">
+      <div tabIndex={0} role="button" className="btn m-1">
+        Theme
+      </div>
       <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
         {themes.map((theme) => (
           <li key={theme}>
@@ -27,7 +29,7 @@ const ThemeSelector = () => {
           </li>
         ))}
       </ul>
-    </details>
+    </div>
   );
 };
 
