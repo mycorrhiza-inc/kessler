7a44c5cc6a82952a9a6fbd8a83a48cc014a43159
Nicole Venner <nvenner@protonmail.ch>
2024-09-15 19:57:22 -0600

    dryify themes

--- frontend/src/components/ThemeSelector.tsx
+++ frontend/src/components/ThemeSelector.tsx
@@ -1,4 +1,5 @@
 const ThemeSelector = () => {
+  const themes = ["Default", "Retro", "Cyberpunk", "Valentine", "Aqua"];
   return (
     <div className="dropdown mb-72">
       <div tabIndex={0} role="button" className="btn m-1">
@@ -17,51 +18,20 @@ const ThemeSelector = () => {
         tabIndex={0}
         className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl"
       >
-        <li>
-          <input
-            type="radio"
-            name="theme-dropdown"
-            className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-            aria-label="Default"
-            value="default"
-          />
-        </li>
-        <li>
-          <input
-            type="radio"
-            name="theme-dropdown"
-            className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-            aria-label="Retro"
-            value="retro"
-          />
-        </li>
-        <li>
-          <input
-            type="radio"
-            name="theme-dropdown"
-            className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-            aria-label="Cyberpunk"
-            value="cyberpunk"
-          />
-        </li>
-        <li>
-          <input
-            type="radio"
-            name="theme-dropdown"
-            className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-            aria-label="Valentine"
-            value="valentine"
-          />
-        </li>
-        <li>
-          <input
-            type="radio"
-            name="theme-dropdown"
-            className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-            aria-label="Aqua"
-            value="aqua"
-          />
-        </li>
+        <ul>
+          {themes.map((theme) => (
+            <li key={theme}>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label={theme}
+                value={theme.toLowerCase()}
+              />
+            </li>
+          ))}
+        </ul>
+        );
       </ul>
     </div>
   );
