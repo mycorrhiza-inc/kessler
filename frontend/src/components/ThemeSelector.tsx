ddd898b447d93aadf257f2325931edb51f043611
Nicole Venner <nvenner@protonmail.ch>
2024-09-16 00:55:18 -0600

    inline theme selector

--- frontend/src/components/ThemeSelector.tsx
+++ frontend/src/components/ThemeSelector.tsx
@@ -10,23 +10,38 @@ const ThemeSelector = () => {
     "aqua",
   ];
   return (
-    <div className="join join-vertical">
-      {themes.map((theme) => (
-        <input
-          type="radio"
-          name="theme-buttons"
-          className="btn theme-controller join-item"
-          aria-label={theme.charAt(0).toUpperCase() + theme.slice(1)}
-          value={theme.toLowerCase()}
-        />
-      ))}
-      <input
-        type="radio"
-        name="theme-buttons"
-        className="btn theme-controller join-item"
-        aria-label="Aqua"
-        value="aqua"
-      />
+    <div className="dropdown mb-72" style={{ zIndex: 3001 }}>
+      <div tabIndex={0} role="button" className="btn m-1">
+        Theme
+        <svg
+          width="12px"
+          height="12px"
+          className="inline-block h-2 w-2 fill-current opacity-60"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 2048 2048"
+        >
+          <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl"
+      >
+        <ul>
+          {themes.map((theme) => (
+            <li key={theme}>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                aria-label={theme.charAt(0).toUpperCase() + theme.slice(1)}
+                value={theme.toLowerCase()}
+              />
+            </li>
+          ))}
+        </ul>
+        );
+      </ul>
     </div>
   );
 };
