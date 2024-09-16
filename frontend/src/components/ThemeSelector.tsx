94d9fc142cce253e8589311b1746d39b3c434d12
Nicole Venner <nvenner@protonmail.ch>
2024-09-16 01:04:51 -0600

    fix header and get working theme selector

--- frontend/src/components/ThemeSelector.tsx
+++ frontend/src/components/ThemeSelector.tsx
@@ -10,40 +10,45 @@ const ThemeSelector = () => {
     "aqua",
   ];
   return (
-    <div className="dropdown mb-72" style={{ zIndex: 3001 }}>
-      <div tabIndex={0} role="button" className="btn m-1">
-        Theme
-        <svg
-          width="12px"
-          height="12px"
-          className="inline-block h-2 w-2 fill-current opacity-60"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 2048 2048"
-        >
-          <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-        </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl"
-      >
-        <ul>
-          {themes.map((theme) => (
-            <li key={theme}>
-              <input
-                type="radio"
-                name="theme-dropdown"
-                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                aria-label={theme.charAt(0).toUpperCase() + theme.slice(1)}
-                value={theme.toLowerCase()}
-              />
-            </li>
-          ))}
-        </ul>
-        );
+    <details className="dropdown">
+      <summary className="btn m-1">Theme</summary>
+      <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+        {themes.map((theme) => (
+          <li key={theme}>
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+              aria-label={theme.charAt(0).toUpperCase() + theme.slice(1)}
+              value={theme.toLowerCase()}
+            />
+          </li>
+        ))}
       </ul>
-    </div>
+    </details>
   );
 };
 
+//<div className="dropdown mb-72" style={{ zIndex: 3001 }}>
+//  <div tabIndex={0} role="button" className="btn m-1">
+//    Theme
+//  </div>
+//  <ul
+//    tabIndex={0}
+//    className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl"
+//  >
+//    {themes.map((theme) => (
+//      <li key={theme}>
+//        <input
+//          type="radio"
+//          name="theme-dropdown"
+//          className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+//          aria-label={theme.charAt(0).toUpperCase() + theme.slice(1)}
+//          value={theme.toLowerCase()}
+//        />
+//      </li>
+//    ))}
+//    );
+//  </ul>
+//</div>
 export default ThemeSelector;
