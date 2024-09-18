5cb6a0f1845f7c8eb9e2a3cd13360fb30abb9755
Nicole Venner <nvenner@protonmail.ch>
2024-09-18 14:00:17 -0600

    fix broken images

--- frontend/src/components/landing/Features.tsx
+++ frontend/src/components/landing/Features.tsx
@@ -11,49 +11,49 @@ type Feature = {
 const featuresData: Feature[] = [
   {
     id: 1,
-    icon: "/images/icon/icon-01.svg",
+    icon: "/landing/images/icon/icon-01.svg",
     title: "Extensive Government Document Database",
     description:
       "Search through a vast collection of over 10,000 government documents, from safety reports to public comments on zoning changes.",
   },
   {
     id: 2,
-    icon: "/images/icon/icon-02.svg",
+    icon: "/landing/images/icon/icon-02.svg",
     title: "Advanced Search and Filtering",
     description:
       "Utilize advanced search capabilities identical to most government dockets, including search by title and filter by author.",
   },
   {
     id: 3,
-    icon: "/images/icon/icon-03.svg",
+    icon: "/landing/images/icon/icon-03.svg",
     title: "Modern User Interface",
     description:
       "Experience a user interface that is intuitive and modern, far from the .ASP forms that havent been updated since 2003.",
   },
   {
     id: 4,
-    icon: "/images/icon/icon-03.svg",
+    icon: "/landing/images/icon/icon-03.svg",
     title: "Multi-format Ingestion",
     description:
       "Ingest a variety of document formats such as PDFs, audio recordings, Word documents, and eBooks.Upload your own documents in bulk and integrate them seamlessly with all existing features.",
   },
   {
     id: 5,
-    icon: "/images/icon/icon-04.svg",
+    icon: "/landing/images/icon/icon-04.svg",
     title: "Full-text Search",
     description:
       "Perform full-text searches across all ingested documents for comprehensive data retrieval.",
   },
   {
     id: 6,
-    icon: "/images/icon/icon-05.svg",
+    icon: "/landing/images/icon/icon-05.svg",
     title: "AI-powered Interaction",
     description:
       "Engage with the docket using a chatbot that leverages Large Language Models (LLMs) and Retrieval-Augmented Generation (RAG).",
   },
   {
     id: 7,
-    icon: "/images/icon/icon-06.svg",
+    icon: "/landing/images/icon/icon-06.svg",
     title: "Continuous Improvements",
     description:
       " In addition to adding new features our goal is to eventually include every publicly accessible government document in our database. Stay tuned for more updates! ",
@@ -63,28 +63,28 @@ const featuresData: Feature[] = [
 const futureFeaturesData: Feature[] = [
   {
     id: 9,
-    icon: "/images/icon/icon-09.svg",
+    icon: "/landing/images/icon/icon-09.svg",
     title: "Document Sharing and Revenue Split",
     description:
       "Share your government documents with other users and earn revenue from making that data accessible.",
   },
   {
     id: 10,
-    icon: "/images/icon/icon-10.svg",
+    icon: "/landing/images/icon/icon-10.svg",
     title: "Author Submission Correlation",
     description:
       "Correlate submissions by a single author across multiple document databases and jurisdictions.",
   },
   {
     id: 11,
-    icon: "/images/icon/icon-11.svg",
+    icon: "/landing/images/icon/icon-11.svg",
     title: "Smart Notifications",
     description:
       "Receive notifications only for important new submissions, ensuring you stay updated without the noise.",
   },
   {
     id: 12,
-    icon: "/images/icon/icon-12.svg",
+    icon: "/landing/images/icon/icon-12.svg",
     title: "Sentiment Analysis",
     description:
       "Easily identify whether documents support or oppose a certain proposal with our sentiment analysis feature.",
@@ -137,8 +137,8 @@ const Feature = () => {
             <h2 className="text-4xl font-bold">SOLID FEATURES</h2>
             <h3 className="text-xl mt-4">Core Features of Kessler</h3>
             <p className="mt-2">
-              The software is still in early alpha, but this is what we have
-              implemented so far.
+              Kessler is still in beta, but this is what we have implemented so
+              far.
             </p>
           </div>
           {/* <!-- Section Title End --> */}
