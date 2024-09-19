a0396f2f1464a16be56d16ffd6ade1cd5ea74776
M Bright <mbright@mycor.io>
2024-09-18 20:35:00 -0600

    fix naming of files and tables

--- backend-go/data/queries/individuals_factions.sql
+++ backend-go/data/queries/individuals_factions.sql
@@ -1,5 +1,5 @@
 -- name: CreateIndividualsInFaction :one
-INSERT INTO public.relation_individuals_in_faction (
+INSERT INTO public.relation_individual_faction (
 		faction_id,
 		individual_id,
 		created_at,
@@ -9,14 +9,14 @@ VALUES ($1, $2, NOW(), NOW())
 RETURNING id;
 -- name: ReadIndividualsInFaction :one
 SELECT *
-FROM public.relation_individuals_in_faction
+FROM public.relation_individual_faction
 WHERE id = $1;
 -- name: ListIndividualsInFaction :many
 SELECT *
-FROM public.relation_individuals_in_faction
+FROM public.relation_individual_faction
 ORDER BY created_at DESC;
 -- name: UpdateIndividualsInFaction :one
-UPDATE public.relation_individuals_in_faction
+UPDATE public.relation_individual_faction
 SET faction_id = $1,
 	individual_id = $2,
 	sa_orm_sentinel = $3,
@@ -24,5 +24,5 @@ SET faction_id = $1,
 WHERE id = $4
 RETURNING id;
 -- name: DeleteIndividualsInFaction :one
-DELETE FROM public.relation_individuals_in_faction
+DELETE FROM public.relation_individual_faction
 WHERE id = $1;
