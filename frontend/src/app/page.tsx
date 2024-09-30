2fc36ebcf94b19cdb3d593633222fff3b63f30fc
Nicole Venner <nvenner@protonmail.ch>
2024-09-30 09:25:51 -0600

    remove uneeded imports

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,6 @@
-import FetchDataSteps from "@/components/supabasetutorial/tutorial/fetch-data-steps";
 import { createClient } from "@/utils/supabase/server";
-import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
-import PlanetStartPage from "@/components/experiments/PlanetHomepage";
-import { exampleArticle } from "@/utils/interfaces";
 import SearchApp from "@/components/SearchApp";
 export default async function ProtectedPage() {
   const supabase = createClient();
