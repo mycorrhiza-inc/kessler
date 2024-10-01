48c2ade7afa4ecb0fe35a666993018888c510fa7
Nicole Venner <nvenner@protonmail.ch>
2024-10-01 11:56:29 -0600

    fix formatting for json func

--- backend-go/rag/rag_engine.go
+++ backend-go/rag/rag_engine.go
@@ -1,6 +1,9 @@
 package rag
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/mycorrhiza-inc/kessler/backend-go/search"
 	openai "github.com/sashabaranov/go-openai"
 	"github.com/sashabaranov/go-openai/jsonschema"
@@ -22,7 +25,15 @@ var rag_query_func_schema = openai.FunctionDefinition{
 
 // arguments='{"order_id":"order_12345"}',
 func rag_query_func(query_json string) (string, error) {
-	search_query := "get query from json when query_json is formatted like {\"query\": \"what is the capital of the united states\"}"
+	var queryData map[string]string
+	err := json.Unmarshal([]byte(query_json), &queryData)
+	if err != nil {
+		return "", fmt.Errorf("error unmarshaling query_json: %v", err)
+	}
+	search_query, ok := queryData["query"]
+	if !ok {
+		return "", fmt.Errorf("query field is missing in query_json")
+	}
 	search_request := search.SearchRequest{search_query, search.Metadata{}}
 	search_results, err := search.SearchQuickwit(search_request)
 	if err != nil {
