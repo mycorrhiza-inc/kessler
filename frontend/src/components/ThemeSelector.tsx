aeeddabc8f5d3f3afb0aef34bad989a8b4e841bc
M Bright <mbright@mycor.io>
2024-10-03 14:09:09 -0600

    clean up theme selector

--- frontend/src/components/ThemeSelector.tsx
+++ frontend/src/components/ThemeSelector.tsx
@@ -83,26 +83,5 @@ const ThemeSelector = () => {
     </div>
   );
 };
-//<div className="dropdown mb-72" style={{ zIndex: 3001 }}>
-//  <div tabIndex={0} role="button" className="btn m-1">
-//    Theme
-//  </div>
-//  <ul
-//    tabIndex={0}
-//    className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl"
-//  >
-//    {themes.map((theme) => (
-//      <li key={theme}>
-//        <input
-//          type="radio"
-//          name="theme-dropdown"
-//          className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-//          aria-label={theme.charAt(0).toUpperCase() + theme.slice(1)}
-//          value={theme.toLowerCase()}
-//        />
-//      </li>
-//    ))}
-//    );
-//  </ul>
-//</div>
+
 export default ThemeSelector;
