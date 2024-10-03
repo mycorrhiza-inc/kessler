40b5096cbed8b6d3fbd9c1d6ec7dcbe3ee7cf9c9
Nicole Venner <nvenner@protonmail.ch>
2024-10-02 18:13:55 -0600

    add hybrid search

--- backend-go/search/search.go
+++ backend-go/search/search.go
@@ -99,7 +99,7 @@ func errturn(err error) ([]SearchData, error) {
 	return nil, err
 }
 
-func constructMetadataQueryString(filter Metadata) string {
+func constructQuickwitMetadataQueryString(filter Metadata) string {
 	var filterQuery string
 	filters := []string{}
 
@@ -148,7 +148,7 @@ func SearchQuickwit(r SearchRequest) ([]SearchData, error) {
 	}
 
 	queryString := r.Query
-	filtersString := constructMetadataQueryString(r.SearchFilters)
+	filtersString := constructQuickwitMetadataQueryString(r.SearchFilters)
 
 	queryString += filtersString
 
@@ -191,16 +191,53 @@ func SearchQuickwit(r SearchRequest) ([]SearchData, error) {
 		errturn(err)
 	}
 
-	rerankedData, err := rerankSearchResults(data, r.Query)
+	return data, nil
+}
+
+func SearchMilvus(request SearchRequest) ([]SearchData, error) {
+	return []SearchData{}, fmt.Errorf("Not implemented")
+}
+
+type SearchReturn struct {
+	Results []SearchData
+	Error   error
+}
+
+func HybridSearch(request SearchRequest) ([]SearchData, error) {
+	// This could absolutely be dryified
+	chanMilvus := make(chan SearchReturn)
+	chanQuickwit := make(chan SearchReturn)
+	go func() {
+		results, err := SearchMilvus(request)
+		return_results := SearchReturn{results, err}
+		chanMilvus <- return_results
+	}()
+	go func() {
+		results, err := SearchQuickwit(request)
+		return_results := SearchReturn{results, err}
+		chanQuickwit <- return_results
+	}()
+	resultsMilvus := <-chanMilvus
+	resultsQuickwit := <-chanQuickwit
+	if resultsMilvus.Error == nil {
+		fmt.Printf("Milvus returned error: %s", resultsMilvus.Error)
+	}
+	if resultsQuickwit.Error == nil {
+		fmt.Printf("Quickwit returned error: %s", resultsQuickwit.Error)
+	}
+	if resultsMilvus.Error != nil && resultsQuickwit.Error != nil {
+		return []SearchData{}, fmt.Errorf("Both Milvus and Quickwit returned errors. milvus error: %s quickwit error: %s", resultsMilvus.Error, resultsQuickwit.Error)
+	}
+	unrankedResults := append(resultsMilvus.Results, resultsQuickwit.Results...)
+	rerankedData, err := rerankSearchResults(unrankedResults, request.Query)
 	// Fail semi silently and returns the regular unranked results
 	if err != nil {
 		log.Printf("Error reranking results: %s", err)
-		return data, nil
+		return unrankedResults, nil
 	}
 
 	return rerankedData, nil
 }
-
 func FormatSearchResults(searchResults []SearchData, query string) string {
 	searchResultsString := fmt.Sprintf("Query: %s\n", query)
 	for _, result := range searchResults {
