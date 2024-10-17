8c391513dbcdf000ad4589716b3d71a6b8525bd5
Nicole Venner <nvenner@protonmail.ch>
2024-10-16 20:18:43 -0600

    try to implement tracing beam, give up

--- frontend/src/components/landing/Landing.tsx
+++ frontend/src/components/landing/Landing.tsx
@@ -5,6 +5,46 @@ import Pricing from "./Pricing";
 import { SupportedStates } from "@/components/DataSetList";
 import { GiMushroomsCluster } from "react-icons/gi";
 import "./Landing.css";
+
+function LandingFooter({}) {
+  return (
+    <>
+      <footer className="footer bg-base-200 text-base-content p-10">
+        <aside>
+          <GiMushroomsCluster style={{ fontSize: "4em" }} />
+          <p>
+            Mycorrhiza Inc
+            <br />
+            Providing reliable tech since 5 hours ago.
+          </p>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Contact Founders</h6>
+          <a href="mailto:n@mycor.io" className="link link-hover">
+            Nicole Venner
+          </a>
+          <a href="mailto:m@mycor.io" className="link link-hover">
+            Mirri Bright
+          </a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company (Links dont work yet)</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Blogs </a>
+        </nav>
+        {/* <nav> */}
+        {/*   <h6 className="footer-title">Legal</h6> */}
+        {/*   <a className="link link-hover">Terms of use</a> */}
+        {/*   <a className="link link-hover">Privacy policy</a> */}
+        {/*   <a className="link link-hover">Cookie policy</a> */}
+        {/* </nav> */}
+      </footer>
+    </>
+  );
+}
+
 export default function Landing({ user }: { user: User | null }) {
   const isLoggedIn = user ? true : false;
   return (
@@ -16,41 +56,11 @@ export default function Landing({ user }: { user: User | null }) {
           data-theme="light"
         >
           <Feature className="col-span-1 md:col-start-1 md:col-span-2 lg:col-start-2 lg:col-span-5 items-center justify-center" />
+          {/* <SupportedStates className="col-span-3 md:col-start-2 md:col-span-3  sm:col-span-1" /> */}
         </div>
         <SupportedStates className="col-span-3 md:col-start-2 md:col-span-3  sm:col-span-1" />
         <Pricing className="col-span-1 md:col-start-1 md:col-span-2 lg:col-start-2 lg:col-span-5 items-center justify-center" />
-        <footer className="footer bg-base-200 text-base-content p-10">
-          <aside>
-            <GiMushroomsCluster style={{ fontSize: "4em" }} />
-            <p>
-              Mycorrhiza Inc
-              <br />
-              Providing reliable tech since 5 hours ago.
-            </p>
-          </aside>
-          <nav>
-            <h6 className="footer-title">Contact Founders</h6>
-            <a href="mailto:n@mycor.io" className="link link-hover">
-              Nicole Venner
-            </a>
-            <a href="mailto:m@mycor.io" className="link link-hover">
-              Mirri Bright
-            </a>
-          </nav>
-          <nav>
-            <h6 className="footer-title">Company (Links dont work yet)</h6>
-            <a className="link link-hover">About us</a>
-            <a className="link link-hover">Contact</a>
-            <a className="link link-hover">Jobs</a>
-            <a className="link link-hover">Blogs </a>
-          </nav>
-          {/* <nav> */}
-          {/*   <h6 className="footer-title">Legal</h6> */}
-          {/*   <a className="link link-hover">Terms of use</a> */}
-          {/*   <a className="link link-hover">Privacy policy</a> */}
-          {/*   <a className="link link-hover">Cookie policy</a> */}
-          {/* </nav> */}
-        </footer>
+        <LandingFooter />
       </div>
     </>
   );
