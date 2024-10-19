672ee98482537d1200c8052cf2001a7edddc9045
Nicole Venner <nvenner@protonmail.ch>
2024-10-19 07:34:56 -0600

    change logic to work with redirects

--- frontend/src/app/start/page.tsx
+++ frontend/src/app/start/page.tsx
@@ -1,21 +1,14 @@
-import SearchApp from "@/components/SearchApp";
 import Landing from "@/components/landing/Landing";
 
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 export default async function Page() {
   const supabase = createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
-  const isLoggedIn = Boolean(user);
   return (
     <div className="w-full">
-      {isLoggedIn ? (
-        <SearchApp user={user}></SearchApp>
-      ) : (
-        <Landing user={user}></Landing>
-      )}
+      <Landing user={user}></Landing>
     </div>
   );
 }
