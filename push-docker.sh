a077e7aeea0a0071b5eea57cd1cf8b3f87724871
Nicole Venner <nvenner@protonmail.ch>
2024-10-24 04:55:03 -0600

    fix logic bug in incomplete script

--- push-docker.sh
+++ push-docker.sh
@@ -5,12 +5,14 @@
 # 3. cd into the kessler repo at /mycorrhiza/kessler 
 # 4. git pull 
 # 5. Run this script
+set -e
+
 docker build -t fractalhuman1/kessler-frontend:latest --platform linux/amd64 ./frontend/ &&
 docker build -t fractalhuman1/kessler-backend-python:latest --platform linux/amd64 ./backend-python/ &&
 docker build -t fractalhuman1/kessler-backend-go:latest --platform linux/amd64 ./backend-go/ &&
 # docker build -t fractalhuman1/kessler-thaumaturgy-python:latest --platform linux/amd64 ./thaumaturgy-python/
 
-docker push fractalhuman1/kessler-frontend:latest
-docker push fractalhuman1/kessler-backend-python:latest
+docker push fractalhuman1/kessler-frontend:latest &&
+docker push fractalhuman1/kessler-backend-python:latest &&
 docker push fractalhuman1/kessler-backend-go:latest 
 # docker push fractalhuman1/kessler-thaumaturgy-python:latest
