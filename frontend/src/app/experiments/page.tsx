de5ad6af69503dd70b0f0da442920f8598d78199
Nicole Venner <nvenner@protonmail.ch>
2024-10-23 18:07:26 -0600

    working experiments page

--- frontend/src/app/experiments/page.tsx
+++ frontend/src/app/experiments/page.tsx
@@ -1,10 +1,15 @@
 import TestFlowVisuals from "@/components/SeriousGaming/SeriousGameAdmin";
-
+import PlanetStartPage from "@/components/experiments/PlanetHomepage";
 import { createClient } from "@/utils/supabase/server";
 export default async function Page() {
   const supabase = createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
-  return <TestFlowVisuals user={user}></TestFlowVisuals>;
+  return (
+    <div className="flex flex-col justify-center space-y-10">
+      <TestFlowVisuals user={user}></TestFlowVisuals>
+      <PlanetStartPage />
+    </div>
+  );
 }
