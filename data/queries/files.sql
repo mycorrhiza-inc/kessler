637867a50ca7fd23b5c1ac9dce5ca368616c236a
Nicole Venner <nvenner@protonmail.ch>
2024-10-29 11:19:31 -0600

    final typo fixes

--- data/queries/files.sql
+++ data/queries/files.sql
@@ -20,44 +20,6 @@ VALUES (
 		NOW()
 	)
 RETURNING id;
--- name: ReadFile :one
-SELECT *
-FROM public.file
-WHERE id = $1;
--- name: ListFiles :many
-SELECT *
-FROM public.file
-ORDER BY updated_at DESC;
--- TODO: Update these to work with the deletion of filestage
--- -- name: ListUnprocessedFiles :many
--- -- get all files that are not in the completed stage
--- -- use a left join to get the stage status
--- SELECT *
--- FROM public.file
--- 	LEFT JOIN public.stage_log sl ON f.stage_id = sl.stage_id
--- WHERE sl.status != 'completed'
--- ORDER BY sl.created_at DESC;
---
--- -- name: ListUnprocessedFilesPagnated :many
--- SELECT *
--- FROM public.file
--- 	LEFT JOIN public.stage_log sl ON f.stage_id = sl.stage_id
--- WHERE sl.status != 'completed'
--- ORDER BY sl.created_at DESC
--- LIMIT $1 OFFSET $2;
--- name: AddStageLog :one
--- used to log the state of a file processing stage and update filestage status
-WITH inserted_log AS (
-	INSERT INTO public.stage_log (
-			file_id,
-			status,
-			log
-		)
-	VALUES ($1, $2, $3)
-	RETURNING id,
-		file_id,
-		status
-)
 -- name: UpdateFile :one
 UPDATE public.file
 SET extension = $1,
@@ -68,9 +30,30 @@ SET extension = $1,
 	updated_at = NOW()
 WHERE public.file.id = $6
 RETURNING id;
--- name: DeleteFile :exec
-DELETE FROM public.file
+-- name: ReadFile :one
+SELECT *
+FROM public.file
 WHERE id = $1;
+
+-- name: ListFiles :many
+SELECT *
+FROM public.file
+ORDER BY updated_at DESC;
+-- name: AddStageLog :one
+-- used to log the state of a file processing stage and update filestage status
+INSERT INTO public.stage_log (
+    file_id,
+    status,
+    log
+  )
+VALUES ($1, $2, $3)
+RETURNING id,
+  file_id,
+  status;
+
+
+-- name: DeleteFile :exec
+DELETE FROM public.file WHERE id = $1;
 -- name: InsertMetadata :one
 INSERT INTO public.file_metadata (
     id,
