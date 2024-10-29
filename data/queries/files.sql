1a72952b3b4daf7f5a9a82306941fa8a3362a2b2
Nicole Venner <nvenner@protonmail.ch>
2024-10-29 10:47:53 -0600

    finishes sql stuff

--- data/queries/files.sql
+++ data/queries/files.sql
@@ -30,27 +30,28 @@ WHERE id = $1;
 SELECT *
 FROM public.file
 ORDER BY updated_at DESC;
--- name: ListUnprocessedFiles :many
--- get all files that are not in the completed stage
--- use a left join to get the stage status
-SELECT *
-FROM public.file
-	LEFT JOIN public.stage_log sl ON f.stage_id = sl.stage_id
-WHERE sl.status != 'completed'
-ORDER BY sl.created_at DESC;
-
--- name: ListUnprocessedFilesPagnated :many
-SELECT *
-FROM public.file
-	LEFT JOIN public.stage_log sl ON f.stage_id = sl.stage_id
-WHERE sl.status != 'completed'
-ORDER BY sl.created_at DESC
-LIMIT $1 OFFSET $2;
+-- TODO: Update these to work with the deletion of filestage
+-- -- name: ListUnprocessedFiles :many
+-- -- get all files that are not in the completed stage
+-- -- use a left join to get the stage status
+-- SELECT *
+-- FROM public.file
+-- 	LEFT JOIN public.stage_log sl ON f.stage_id = sl.stage_id
+-- WHERE sl.status != 'completed'
+-- ORDER BY sl.created_at DESC;
+--
+-- -- name: ListUnprocessedFilesPagnated :many
+-- SELECT *
+-- FROM public.file
+-- 	LEFT JOIN public.stage_log sl ON f.stage_id = sl.stage_id
+-- WHERE sl.status != 'completed'
+-- ORDER BY sl.created_at DESC
+-- LIMIT $1 OFFSET $2;
 -- name: AddStageLog :one
 -- used to log the state of a file processing stage and update filestage status
 WITH inserted_log AS (
 	INSERT INTO public.stage_log (
-			stage_id,
+			file_id,
 			status,
 			log
 		)
@@ -59,19 +60,13 @@ WITH inserted_log AS (
 		stage_id,
 		status
 )
-UPDATE public.filestage fs
-SET status = il.status
-FROM inserted_log il
-WHERE fs.id = il.stage_id
-RETURNING il.id;
 -- name: UpdateFile :one
 UPDATE public.file
 SET extension = $1,
 	lang = $2,
 	name = $3,
-	stage_id = $4,
-	isPrivate = $5,
-  hash = $6,
+	isPrivate = $4,
+  hash = $5,
 	updated_at = NOW()
 WHERE id = $6
 RETURNING id;
