922d3f9c0d3021af31d1a6510943e09ddb873502
M Bright <mbright@mycor.io>
2024-10-29 20:01:56 -0600

    fixed navbar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,16 +3,27 @@ import Landing from "@/components/landing/Landing";
 
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-export default async function Page() {
+
+import { useKesslerStore } from "@/lib/store";
+import AuthGuard from "@/components/AuthGuard";
+
+export const checkLoggedIn = async () => {
   const supabase = createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
-  const isLoggedIn = Boolean(user);
+  const userPresent = Boolean(user);
+  return userPresent
+};
+
+export default async function Page() {
+  const isLoggedIn = await checkLoggedIn();
   return (
     <div className="w-full">
       {isLoggedIn ? (
-        <SearchApp />
+        <AuthGuard isLoggedIn={isLoggedIn}>
+          <SearchApp />
+        </AuthGuard>
       ) : (
         <Landing />
       )}
--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -1,11 +1,11 @@
 import { create } from 'zustand'
 
-interface BearState {
+interface KesslerState {
   isLoggedIn: boolean
   setIsLoggedIn: (isLoggedIn: boolean) => void
 }
 
-export const useBearStore = create<BearState>()((set) => ({
+export const useKesslerStore = create<KesslerState>()((set) => ({
   isLoggedIn: false,
   setIsLoggedIn: (isLoggedIn: boolean) => set({ isLoggedIn }),
 }))
