3b9e184af76bfdfd0dd5c2211cc8ea6407049648
Nicole Venner <nvenner@protonmail.ch>
2024-11-18 13:32:50 -0700

    add conversations sql

---
+++ data/queries/conversations.sql
@@ -0,0 +1,51 @@
+
+-- name: DocketDocumentInsert :one
+INSERT INTO public.docket_documents (
+		docket_id,
+		file_id,
+		created_at,
+		updated_at
+	)
+VALUES ($1, $2, NOW(), NOW())
+RETURNING docket_id;
+
+-- name: DocketDocumentDeleteAll :exec
+DELETE FROM public.docket_documents
+WHERE docket_id = $1;
+
+-- name: CreateDocketConversation :one
+INSERT INTO public.docket_conversations (
+		docket_id,
+		state,
+		created_at,
+		updated_at
+	)
+VALUES ($1, $2, NOW(), NOW())
+RETURNING id;
+
+-- name: DocketConversationFetchByDocketIdMatch :many
+SELECT *
+FROM public.docket_conversations
+WHERE docket_id = $1;
+
+-- name: ReadDocketConversation :one
+SELECT *
+FROM public.docket_conversations
+WHERE id = $1;
+
+-- name: ListDocketConversations :many
+SELECT *
+FROM public.docket_conversations
+ORDER BY created_at DESC;
+
+-- name: UpdateDocketConversation :one
+UPDATE public.docket_conversations
+SET docket_id = $1,
+	state = $2,
+	updated_at = NOW()
+WHERE id = $3
+RETURNING id;
+
+-- name: DeleteDocketConversation :exec
+DELETE FROM public.docket_conversations
+WHERE id = $1;
