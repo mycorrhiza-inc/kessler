e6f3884f136aa0a1486093d88efe35a5110dfdd2
Nicole Venner <nvenner@protonmail.ch>
2024-11-20 10:44:59 -0700

    add initial convo pages for stuff

--- backend-go/crud/org_convo_readers.go
+++ backend-go/crud/org_convo_readers.go
@@ -1,1 +1,37 @@
 package crud
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/mycorrhiza-inc/kessler/backend-go/gen/dbstore"
+)
+
+func GetOrgWithFiles(dbtx_val dbstore.DBTX) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Getting file with metadata")
+		q := *dbstore.New(dbtx_val)
+		params := mux.Vars(r)
+		orgID := params["uuid"]
+		parsedUUID, err := uuid.Parse(orgID)
+		if err != nil {
+			http.Error(w, "Invalid File ID format", http.StatusBadRequest)
+			return
+		}
+		pgUUID := pgtype.UUID{Bytes: parsedUUID, Valid: true}
+		ctx := r.Context()
+		// TODO: Get these 2 requests to happen in the same query, and or run concurrently
+		org_name, err := q.ReadOrganization(ctx context.Context, id pgtype.UUID)r
+		if err != nil {
+			http.Error(w, "File not found", http.StatusNotFound)
+			return
+		}
+		response, _ := json.Marshal(file)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(response)
+	}
+}
