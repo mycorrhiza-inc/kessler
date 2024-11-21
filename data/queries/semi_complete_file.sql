05b6f7ce9058c5e08cea5e70b2d61175df38ec2f
Nicole Venner <nvenner@protonmail.ch>
2024-11-21 08:56:05 -0700

    cleaned up the queries for both

--- data/queries/semi_complete_file.sql
+++ data/queries/semi_complete_file.sql
@@ -1,17 +1,17 @@
 
 -- name: GetFileWithMetadata :one
-SELECT *
-FROM public.file
-  LEFT JOIN public.file_metadata ON public.file.id = public.file_metadata.id
+SELECT f.*,
+  fm.mdata
+FROM public.file f
+  LEFT JOIN public.file_metadata fm ON public.file.id = public.file_metadata.id
 WHERE public.file.id = $1;
 
--- name: GetSemiCompleteFile : one 
-
+-- name: GetSemiCompleteFile :many
 SELECT 
     f.*,
-    fm.*,
-    fe.*,
-    dd.*,
+    fm.mdata,
+    fe.extra_obj,
+    dd.docket_id as docket_uuid,
     org.id as organization_id,
     org.name as organization_name
 FROM public.file f
@@ -19,5 +19,5 @@ FROM public.file f
     LEFT JOIN public.file_extras fe ON f.id = fe.id
     LEFT JOIN public.docket_documents dd ON f.id = dd.document_id
     LEFT JOIN public.relation_documents_organizations_authorship rdoa ON f.id = rdoa.document_id
-    LEFT JOIN public.organizations org ON rdoa.organization_id = org.id
+    LEFT JOIN public.organization org ON rdoa.organization_id = org.id
 WHERE f.id = $1;
