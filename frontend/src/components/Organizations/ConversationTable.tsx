2007773488e1213168d8add3045e79b858a5d742
Nicole Venner <nvenner@protonmail.ch>
2024-11-21 03:45:26 -0700

    working conversation table

--- frontend/src/components/Organizations/ConversationTable.tsx
+++ frontend/src/components/Organizations/ConversationTable.tsx
@@ -4,14 +4,18 @@ import Link from "next/link";
 import useSWRImmutable from "swr/immutable";
 import LoadingSpinner from "../styled-components/LoadingSpinner";
 
-const conversationsListAll = async (url: string) => {
-  const response = await axios.get(url);
-  console.log(response.data);
-  const return_data: any[] = response.data;
-  if (return_data.length == 0 || return_data == undefined) {
-    return [];
-  }
-  return return_data;
+const conversationsListAll = (redundant_key: string) => {
+  const cleanData = (response: any) => {
+    console.log(response.data);
+    const return_data: any[] = response.data;
+    if (return_data.length == 0 || return_data == undefined) {
+      return [];
+    }
+    return return_data;
+  };
+  return axios
+    .get(`${apiURL}/v2/public/conversations/list`)
+    .then((res) => cleanData(res));
 };
 
 const ConversationTable = () => {
@@ -20,27 +24,32 @@ const ConversationTable = () => {
     conversationsListAll,
   );
   const convoList = data;
-  console.log("convo list: " + convoList);
+  console.log("Convo List:", convoList);
   return (
     <>
       {isLoading && <LoadingSpinner loadingText="Loading Conversations" />}
       {error && <p>Failed to load conversations {error}</p>}
-      {!isLoading && !error && (
+      {!isLoading && !error && convoList != undefined && (
         <table className="table table-pin-rows">
           <thead>
             <tr>
               <td>Name</td>
-              <td>Description</td>
+              <td>State</td>
             </tr>
           </thead>
           <tbody>
             {convoList.map((convo: any) => (
-              <Link href={`/proceedings/${convo.name}`}>
-                <tr>
-                  <td>{convo.name}</td>
-                  <td>{convo.description}</td>
-                </tr>
-              </Link>
+              <tr key={convo.DocketID}>
+                <td colSpan={2} className="p-0">
+                  <Link
+                    href={`/proceedings/${convo.DocketID}`}
+                    className="flex w-full"
+                  >
+                    <div className="flex-1 px-4 py-3">{convo.DocketID}</div>
+                    <div className="flex-1 px-4 py-3">{convo.State}</div>
+                  </Link>
+                </td>
+              </tr>
             ))}
           </tbody>
         </table>
