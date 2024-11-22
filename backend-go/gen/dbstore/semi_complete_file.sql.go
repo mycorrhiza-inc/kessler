ed9bf0e7901ef122075e9141299b9a7036b0371b
Nicole Venner <nvenner@protonmail.ch>
2024-11-21 21:05:49 -0700

    remove renames causing issues + typo

--- backend-go/gen/dbstore/semi_complete_file.sql.go
+++ backend-go/gen/dbstore/semi_complete_file.sql.go
@@ -60,26 +60,26 @@ func (q *Queries) GetFileWithMetadata(ctx context.Context, id pgtype.UUID) (GetF
 
 const semiCompleteFileGet = `-- name: SemiCompleteFileGet :many
 SELECT 
-    f.id,
-    f.name,
-    f.extension,
-    f.lang,
-    f.verified,
-    f.hash,
-    f.created_at,
-    f.updated_at,
-    fm.mdata,
-    fe.extra_obj,
-    dd.docket_id as docket_uuid,
-    org.id as organization_id,
-    org.name as organization_name
-FROM public.file f
-    LEFT JOIN public.file_metadata fm ON f.id = fm.id
-    LEFT JOIN public.file_extras fe ON f.id = fe.id
-    LEFT JOIN public.docket_documents dd ON f.id = dd.document_id
-    LEFT JOIN public.relation_documents_organizations_authorship rdoa ON f.id = rdoa.document_id
-    LEFT JOIN public.organization org ON rdoa.organization_id = org.id
-WHERE f.id = $1
+    public.file.id,
+    public.file.name,
+    public.file.extension,
+    public.file.lang,
+    public.file.verified,
+    public.file.hash,
+    public.file.created_at,
+    public.file.updated_at,
+    public.file_metadata.mdata,
+    public.file_extras.extra_obj,
+    public.docket_documents.docket_id as docket_uuid,
+    public.organization.id as organization_id,
+    public.organization.name as organization_name
+FROM public.file
+    LEFT JOIN public.file_metadata ON public.file.id = public.file_metadata.id
+    LEFT JOIN public.file_extras ON public.file.id = public.file_extras.id
+    LEFT JOIN public.docket_documents ON public.file.id = public.docket_documents.file_id
+    LEFT JOIN public.relation_documents_organizations_authorship ON public.file.id = public.relation_documents_organizations_authorship.document_id
+    LEFT JOIN public.organization ON public.relation_documents_organizations_authorship.organization_id = public.organization.id
+WHERE public.file.id = $1
 `
 
 type SemiCompleteFileGetRow struct {
