ae68736c54b55c84ab03fc964b56c7207d7d02de
Nicole Venner <nvenner@protonmail.ch>
2024-11-26 19:24:05 -0700

    moar stuff for cleaning orgs

--- backend-go/crud/cleaning_scripts.go
+++ backend-go/crud/cleaning_scripts.go
@@ -2,6 +2,7 @@ package crud
 
 import (
 	"context"
+	"strings"
 
 	"github.com/mycorrhiza-inc/kessler/backend-go/gen/dbstore"
 )
@@ -17,11 +18,17 @@ func deduplicateAllOrganizationsInitial(ctx context.Context, q dbstore.Queries, 
 
 	for _, org := range all_orgs {
 		orgname := org.Name
+		orgNameTrimmed := strings.TrimSpace(orgname)
+		if orgNameTrimmed != orgname {
+			org.Name = orgNameTrimmed
+			args := dbstore.OrganizationUpdateParams{ID: org.ID, Name: orgNameTrimmed, IsPerson: org.IsPerson, Description: org.Description}
+			q.OrganizationUpdate(ctx, args)
+		}
 		if old_org, exists := orgMap[orgname]; exists {
 			// Found a duplicate organization name
 			args := dbstore.OrganizationDeduplicateCascadeParams{
-				ID:             old_org.ID, // Org to be deleted
-				OrganizationID: org.ID,     // Org to be kept
+				ID:             org.ID,     // Org to be deleted
+				OrganizationID: old_org.ID, // Org to be kept
 			}
 
 			err := q.OrganizationDeduplicateCascade(ctx, args)
