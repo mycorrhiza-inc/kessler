189ca8157e95ee985c502cd4593bb3e722c32faa
Nicole Venner <nvenner@protonmail.ch>
2024-11-30 12:26:30 -0700

    redo routing

---
+++ backend-go/admin/docproc_get.go
@@ -0,0 +1,95 @@
+package admin
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/mycorrhiza-inc/kessler/backend-go/crud"
+	"github.com/mycorrhiza-inc/kessler/backend-go/gen/dbstore"
+)
+
+func UnverifedCompleteFileSchemaListFactory(dbtx_val dbstore.DBTX) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		q := *dbstore.New(dbtx_val)
+		params := mux.Vars(r)
+		max_responses_str := params["max_responses"]
+		max_responses, err := strconv.Atoi(max_responses_str)
+		if err != nil || max_responses < 0 {
+			errorstring := fmt.Sprintf("Error parsing max responses: %v", err)
+			fmt.Println(errorstring)
+			http.Error(w, errorstring, http.StatusBadRequest)
+			return
+		}
+		files, err := UnverifedCompleteFileSchemaList(r.Context(), q, uint(max_responses))
+		if err != nil {
+			errorstring := fmt.Sprintf("Error getting unverified files: %v", err)
+			fmt.Println(errorstring)
+			http.Error(w, errorstring, http.StatusInternalServerError)
+			return
+		}
+		response, _ := json.Marshal(files)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(response)
+	}
+}
+
+func UnverifedCompleteFileSchemaList(ctx context.Context, q dbstore.Queries, max_responses uint) ([]crud.CompleteFileSchema, error) {
+	files, err := q.FilesListUnverified(ctx)
+	if err != nil {
+		return []crud.CompleteFileSchema{}, err
+	}
+	unverified_raw_uuids := make([]uuid.UUID, len(files))
+	for i, file := range files {
+		unverified_raw_uuids[i] = file.ID
+	}
+	// Shuffle the uuids around to get a random selection while processing
+	for i := range unverified_raw_uuids {
+		j := rand.Intn(i + 1) // Want to get a range of [0,i] so that there is a possibility of the null swap.
+		// Inductive proof this distributes the elements randomly at step k:
+		// The element at index k is evenly distributed, since it has a 1/k chance of being the element at k, and a k-1/k chance of selecting from an even distribution of k-1 elements, thus meaning it has an even distribution of 1/k probability of selecting k elements.
+		// Same thing for other elements, it has a k-1/k chance of sampling from its EXISTING even distribution of k-1 elements, and a 1/k chance of swapping with the k'th element. Thus it has a even 1/k chance of being selected from every element.
+		unverified_raw_uuids[i], unverified_raw_uuids[j] = unverified_raw_uuids[j], unverified_raw_uuids[i]
+	}
+	if len(unverified_raw_uuids) > int(max_responses) {
+		unverified_raw_uuids = unverified_raw_uuids[:max_responses]
+	}
+
+	complete_files := []crud.CompleteFileSchema{}
+	fileChan := make(chan crud.CompleteFileSchema)
+	errChan := make(chan error)
+	var wg sync.WaitGroup
+
+	for _, uuid := range unverified_raw_uuids {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			complete_file, err := crud.CompleteFileSchemaGetFromUUID(ctx, q, uuid)
+			if err != nil {
+				fmt.Printf("Error getting file %v: %v\n", uuid, err)
+				// errChan <- err
+				return
+			}
+			fileChan <- complete_file
+		}()
+	}
+
+	// Close channels when all goroutines complete
+	go func() {
+		wg.Wait()
+		close(fileChan)
+		close(errChan)
+	}()
+
+	// Collect results
+	for file := range fileChan {
+		complete_files = append(complete_files, file)
+	}
+	return complete_files, nil
+}
--- backend-go/crud/file_read_handler.go
+++ backend-go/crud/file_read_handler.go
@@ -5,10 +5,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"math/rand"
 	"net/http"
 	"os"
-	"sync"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -181,60 +179,6 @@ func CompleteFileSchemaGetFromUUID(ctx context.Context, q dbstore.Queries, uuid 
 	}
 	file.Stage = stage
 	return file, nil
-}
-
-func UnverifedCompleteFileSchemaList(ctx context.Context, q dbstore.Queries, max_responses uint) ([]CompleteFileSchema, error) {
-	files, err := q.FilesListUnverified(ctx)
-	if err != nil {
-		return []CompleteFileSchema{}, err
-	}
-	unverified_raw_uuids := make([]uuid.UUID, len(files))
-	for i, file := range files {
-		unverified_raw_uuids[i] = file.ID
-	}
-	// Shuffle the uuids around to get a random selection while processing
-	for i := range unverified_raw_uuids {
-		j := rand.Intn(i + 1) // Want to get a range of [0,i] so that there is a possibility of the null swap.
-		// Inductive proof this distributes the elements randomly at step k:
-		// The element at index k is evenly distributed, since it has a 1/k chance of being the element at k, and a k-1/k chance of selecting from an even distribution of k-1 elements, thus meaning it has an even distribution of 1/k probability of selecting k elements.
-		// Same thing for other elements, it has a k-1/k chance of sampling from its EXISTING even distribution of k-1 elements, and a 1/k chance of swapping with the k'th element. Thus it has a even 1/k chance of being selected from every element.
-		unverified_raw_uuids[i], unverified_raw_uuids[j] = unverified_raw_uuids[j], unverified_raw_uuids[i]
-	}
-	if len(unverified_raw_uuids) > int(max_responses) {
-		unverified_raw_uuids = unverified_raw_uuids[:max_responses]
-	}
-
-	complete_files := []CompleteFileSchema{}
-	fileChan := make(chan CompleteFileSchema)
-	errChan := make(chan error)
-	var wg sync.WaitGroup
-
-	for _, uuid := range unverified_raw_uuids {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			complete_file, err := CompleteFileSchemaGetFromUUID(ctx, q, uuid)
-			if err != nil {
-				fmt.Printf("Error getting file %v: %v\n", uuid, err)
-				// errChan <- err
-				return
-			}
-			fileChan <- complete_file
-		}()
-	}
-
-	// Close channels when all goroutines complete
-	go func() {
-		wg.Wait()
-		close(fileChan)
-		close(errChan)
-	}()
-
-	// Collect results
-	for file := range fileChan {
-		complete_files = append(complete_files, file)
-	}
-	return complete_files, nil
 }
 
 func ReadFileHandlerFactory(config FileHandlerConfig) http.HandlerFunc {
