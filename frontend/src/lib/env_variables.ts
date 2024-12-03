e3804f78aea72e6750fdf270517f4c9ae968b04d
Nicole Venner <nvenner@protonmail.ch>
2024-12-03 12:45:59 -0700

    refactor org page to use react server components

--- frontend/src/lib/env_variables.ts
+++ frontend/src/lib/env_variables.ts
@@ -1,1 +1,4 @@
-export const apiURL = process.env.NEXT_PUBLIC_KESSLER_API_URL || "https://api.kessler.xyz";
+export const apiURL =
+  process.env.NEXT_PUBLIC_KESSLER_API_URL || "https://api.kessler.xyz";
+export const prodAPIURL = "https://api.kessler.xyz";
+
--- frontend/src/lib/requests/organizations.ts
+++ frontend/src/lib/requests/organizations.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { apiURL } from "../env_variables";
+import { apiURL, prodAPIURL } from "../env_variables";
 
 export const getOrganizationInfo = async (orgID: string) => {
   const response = await axios.get(
-    `${apiURL}/v2/public/organizations/${orgID}`,
+    // `${apiURL}/v2/public/organizations/${orgID}`,
+    `${prodAPIURL}/v2/public/organizations/${orgID}`,
     // "http://api.kessler.xyz/v2/recent_updates",
   );
   console.log("organization data", response.data);
