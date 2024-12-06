9af78ca767dd0421f8092491a1188cd3b6c3f8be
Nicole Venner <nvenner@protonmail.ch>
2024-12-05 19:07:55 -0700

    implement date ingest for quickwit

--- backend-go/quickwit/quickwit.go
+++ backend-go/quickwit/quickwit.go
@@ -153,6 +153,21 @@ func ResolveFileSchemaForDocketIngest(complete_files []files.CompleteFileSchema)
 			author_uuids += author.AuthorID.String() + " "
 		}
 		metadata["author_uuids"] = author_uuids
+		// FIXME: IMPLEMENT A date_published FIELD IN PG AND RENDER THIS BASED ON THAT
+		dateStr := metadata["date"].(string)
+		if dateStr != "" {
+			dateParts := strings.Split(dateStr, "/")
+			if len(dateParts) == 3 {
+				month := dateParts[0]
+				day := dateParts[1]
+				year := dateParts[2]
+
+				parsedDate, err := time.Parse("01/02/2006", fmt.Sprintf("%s/%s/%s", month, day, year))
+				if err == nil {
+					metadata["date_filed"] = parsedDate.Format(time.RFC3339)
+				}
+			}
+		}
 		return metadata
 	}
 	var data []QuickwitFileUploadData
