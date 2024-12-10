5f2f3bd6bfaf9c5af69855c217ae4d57b259736d
Nicole Venner <nvenner@protonmail.ch>
2024-12-09 22:19:50 -0700

    refactor api urls

--- frontend/src/lib/env_variables.ts
+++ frontend/src/lib/env_variables.ts
@@ -1,4 +1,10 @@
-export const apiURL =
+// This url is for hitting the kessler api from the client,
+export const publicAPIURL =
   process.env.NEXT_PUBLIC_KESSLER_API_URL || "https://api.kessler.xyz";
-export const prodAPIURL = "https://api.kessler.xyz";
-
+// This is the url for hitting the api, for internal use by the nextjs server runtime, primarially for SSR pages.
+// This defaults to the prod url for now, since setting it to localhost doesnt work because the api isnt accessible from the localhost of
+// the nextjs docker container.
+// TODO: Figure out why replacing this with http://backend-go:4041 doesnt work.
+// which would improve security and prevent a network route outside the system for SSR.
+export const internalAPIURL =
+  process.env.INTERNAL_KESSLER_API_URL || "http://api.kessler.xyz";
--- frontend/src/lib/requests/organizations.ts
+++ frontend/src/lib/requests/organizations.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { apiURL, prodAPIURL } from "../env_variables";
+import { publicAPIURL, internalAPIURL } from "../env_variables";
 
 export const getOrganizationInfo = async (orgID: string) => {
   const response = await axios.get(
-    // `${apiURL}/v2/public/organizations/${orgID}`,
-    `${prodAPIURL}/v2/public/organizations/${orgID}`,
+    // `${publicAPIURL}/v2/public/organizations/${orgID}`,
+    `${internalAPIURL}/v2/public/organizations/${orgID}`,
     // "http://api.kessler.xyz/v2/recent_updates",
   );
   console.log("organization data", response.data);
