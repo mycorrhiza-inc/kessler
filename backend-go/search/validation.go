0dd91b879f5a541ef94c94b67868d5d67e604ebe
Nicole Venner <nvenner@protonmail.ch>
2024-12-19 07:06:09 -0700

    moar fixes

--- backend-go/search/validation.go
+++ backend-go/search/validation.go
@@ -8,41 +8,33 @@ import (
 )
 
 func SearchDataPassesFilters(result SearchDataHydrated, filters networking.FilterFields) bool {
-	mdata := result.File.Mdata
+	// mdata := result.File.Mdata
 	docket_id := filters.MetadataFilters.DocketID
 	if result.File.Conversation.DocketID != docket_id {
 		return false
 	}
-	file_class := filters.MetadataFilters.FileClass
-	if mdata["file_class"] != file_class {
-		return false
-	}
-	// author := filters.MetadataFilters.Author
-	// if result.File.Author != author {
+	// file_class := filters.MetadataFilters.FileClass
+	// if mdata["file_class"] != file_class {
 	// 	return false
 	// }
 	// doctype := filters.MetadataFilters.Doctype
-	// if result.File.Doctype != doctype {
-	// 	return false
-	// }
-	// lang := filters.MetadataFilters.Lang
-	// if result.File.Language != lang {
+	// if result.File.Extension != doctype {
 	// 	return false
 	// }
 
 	return true
 }
 
-func ValidateSearchResultsAgainstFilters(ctx context.Context, results []SearchData, filters networking.FilterFields) error {
+func ValidateSearchResultsAgainstFilters(ctx context.Context, results []SearchData, filters networking.FilterFields) ([]SearchDataHydrated, error) {
 	q := *routing.DBQueriesFromContext(ctx)
 	hydrated_results, err := HydrateSearchResults(results, ctx, q)
 	if err != nil {
-		return err
+		return []SearchDataHydrated{}, err
 	}
 	return ValidateSearchResultsHydratedAgainstFilters(hydrated_results, filters)
 }
 
-func ValidateSearchResultsHydratedAgainstFilters(results []SearchDataHydrated, filters networking.FilterFields) error {
+func ValidateSearchResultsHydratedAgainstFilters(results []SearchDataHydrated, filters networking.FilterFields) ([]SearchDataHydrated, error) {
 	invalid_count := 0
 	for i, result := range results {
 		if !SearchDataPassesFilters(result, filters) {
@@ -51,7 +43,7 @@ func ValidateSearchResultsHydratedAgainstFilters(results []SearchDataHydrated, f
 		}
 	}
 	if invalid_count > 0 {
-		return fmt.Errorf("Found %v invalid results out of %v total results", invalid_count, len(results))
+		return results, fmt.Errorf("Found %v invalid results out of %v total results", invalid_count, len(results))
 	}
-	return nil
+	return results, nil
 }
