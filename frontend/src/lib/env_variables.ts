ed46bf59f7eb35fbd2af3570637232d0ddcb8fe2
Nicole Venner <nvenner@protonmail.ch>
2024-12-21 15:17:11 -0700

    if running locally always enable experimental settings

--- frontend/src/lib/env_variables.ts
+++ frontend/src/lib/env_variables.ts
@@ -1,6 +1,7 @@
 // This url is for hitting the kessler api from the client,
 export const publicAPIURL =
   process.env.NEXT_PUBLIC_KESSLER_API_URL || "https://api.kessler.xyz";
+export const isLocalMode = publicAPIURL.indexOf("localhost") !== -1;
 // This is the url for hitting the api, for internal use by the nextjs server runtime, primarially for SSR pages.
 // This defaults to the prod url for now, since setting it to localhost doesnt work because the api isnt accessible from the localhost of
 // the nextjs docker container.
