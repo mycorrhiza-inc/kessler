1986f7b44539ba325c42f24ffe37fcb9cefca569
Nicole Venner <nvenner@protonmail.ch>
2024-12-22 08:19:00 -0700

    force markdown page into dynamic mode for content rendering

--- frontend/src/app/(application)/admin-testing/markdown-test/page.tsx
+++ frontend/src/app/(application)/admin-testing/markdown-test/page.tsx
@@ -3,12 +3,15 @@ import MarkdownRenderer, {
   testMarkdownContent,
 } from "@/components/MarkdownRenderer";
 import PageContainer from "@/components/Page/PageContainer";
-
+import { ForceUseClient } from "@/components/UtilityComponents";
+export const dynamic = "force-dynamic";
 export default function Page() {
   return (
     <>
       <PageContainer breadcrumbs={{ breadcrumbs: [] }}>
-        <MarkdownRenderer>{testMarkdownContent}</MarkdownRenderer>
+        <ForceUseClient>
+          <MarkdownRenderer>{testMarkdownContent}</MarkdownRenderer>
+        </ForceUseClient>
       </PageContainer>
     </>
   );
