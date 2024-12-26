358e18bf1e09a55cc6e4cb09ec971d179a9c6003
Nicole Venner <nvenner@protonmail.ch>
2024-12-26 10:23:36 -0700

    change container backends

--- k8s/install-k8s-initial.sh
+++ k8s/install-k8s-initial.sh
@@ -4,23 +4,37 @@ chsh -s /usr/bin/fish
 git clone https://github.om/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
-# Install docker
-dnf -y install dnf-plugins-core
-dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
-
-
+mkdir /root/install-artifacts
+cd /root/install-artifacts
 # Install k8s and helm
 dnf install -y kubernetes1.32 kubernetes1.32-kubeadm kubernetes1.30-client 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-# Install a docker shim, apparently it lets k8s control docker
-wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.16/cri-dockerd-0.3.16-3.fc36.x86_64.rpm
-dnf install -y ./cri-dockerd-0.3.16-3.fc36.x86_64.rpm
+# Apparently docker makes offical changes to the Open Container Initiative (OCI) standard. So we are not using docker here
+wget https://github.com/containerd/containerd/releases/download/v2.0.1/containerd-2.0.1-linux-amd64.tar.gz
+tar Cxzvf /usr/local containerd-*
+systemctl daemon-reload
+systemctl enable --now containerd
+
+wget https://github.com/opencontainers/runc/releases/download/v1.2.3/runc.amd64
+install -m 755 runc.amd64 /usr/local/sbin/runc
+
+wget https://github.com/containernetworking/plugins/releases/download/v1.6.1/cni-plugins-linux-amd64-v1.6.1.tgz
+mkdir -p /opt/cni/bin
+tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.6.1.tgz
+# # Install docker
+# dnf -y install dnf-plugins-core
+# dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+# dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# systemctl start docker
+# systemctl enable docker
+#
+#
+# # Install a docker shim, apparently it lets k8s control docker
+# wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.16/cri-dockerd-0.3.16-3.fc36.x86_64.rpm
+# dnf install -y ./cri-dockerd-0.3.16-3.fc36.x86_64.rpm
 
 cd / 
 mkdir mycorrhiza
