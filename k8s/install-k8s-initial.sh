c79a2ef2f976526fb0cac56f6258d92479d65ba8
Nicole Venner <nvenner@protonmail.ch>
2024-12-26 13:02:31 -0700

    initial commit before trying to get ssl certs working

--- k8s/install-k8s-initial.sh
+++ k8s/install-k8s-initial.sh
@@ -2,50 +2,39 @@ set -euo pipefail
 # Install utility packages to make debugging on the server easier.
 dnf install git fish tmux micro neovim btop wget -y
 chsh -s /usr/bin/fish
-git clone https://github.om/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
+# git clone https://github.com/LazyVim/starter ~/.config/nvim
+# rm -rf ~/.config/nvim/.git
 
-mkdir /root/install-artifacts
-cd /root/install-artifacts
-# Install k8s and helm
-dnf install -y kubernetes1.32 kubernetes1.32-kubeadm kubernetes1.30-client 
+
+
+
+# Trying something different, k8s is seeminly hugely overkill, and all the interoperable components 
+# I feel could create a complexity nightmare, trying something a bit simpler with a regular k3s install 
+curl -sfL https://get.k3s.io | sh -
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-systemctl enable containerd
-systemctl start containerd
-# Enable IP forwarding
-echo "1" > /proc/sys/net/ipv4/ip_forward
-# Make IP forwarding persistent across reboots
-echo "net.ipv4.ip_forward = 1" > /etc/sysctl.d/99-kubernetes-cri.conf
-sysctl --system
-
-# this works
-kubeadm init
-
-# Then I try this and get an error
 helm list
-# Error: Kubernetes cluster unreachable: Get "http://localhost:8080/version": dial tcp [::1]:8080: connect: connection refused
 
-# # Apparently docker makes offical changes to the Open Container Initiative (OCI) standard. So we are not using docker here
-# wget https://github.com/containerd/containerd/releases/download/v2.0.1/containerd-2.0.1-linux-amd64.tar.gz
-# tar Cxzvf /usr/local containerd-*
-# systemctl daemon-reload
-# systemctl enable --now containerd
-#
-# wget https://github.com/opencontainers/runc/releases/download/v1.2.3/runc.amd64 e
-# install -m 755 runc.amd64 /usr/local/sbin/runc
-#
-# wget https://github.com/containernetworking/plugins/releases/download/v1.6.1/cni-plugins-linux-amd64-v1.6.1.tgz
-# mkdir -p /opt/cni/bin
-# tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.6.1.tgz
-# # Although apparently you can also install it with dnf using the containerd distributed by docker like so
-# dnf -y install dnf-plugins-core
-# dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-# dnf install containerd.io  -y
-# systemctl daemon-reload
-# systemctl enable --now containerd
+
+# Install k8s dashboard
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm install testing-dashboard kubernetes-dashboard/kubernetes-dashboard
+# To make acessible publicly
+# kubectl -n default port-forward svc/test-dashboard-kong-proxy 8443:443 --address 0.0.0.0
+
+# Create a service account with cluster-admin privileges
+kubectl create serviceaccount nicole -n default
+kubectl create clusterrolebinding nicole-admin-binding \
+    --clusterrole=cluster-admin \
+    --serviceaccount=default:nicole
+
+# Create a token for the service account
+kubectl create token nicole
+
 
 cd / 
 mkdir mycorrhiza
@@ -58,4 +47,32 @@ git fetch --all
 
 
 # manually do some magic to copy k8s/secret.yml
-# helm install kessler ./k8s -f k8s/values-prod.yaml
+helm install kessler ./k8s -f k8s/values-prod.yaml
+
+
+
+
+
+# Now the service account 'nicole' has full cluster administration privileges
+# WARNING: This gives complete unrestricted access - only use in test environments!
+#
+# mkdir /root/install-artifacts
+# cd /root/install-artifacts
+# # Install k8s and helm
+# dnf install -y kubernetes1.32 kubernetes1.32-kubeadm kubernetes1.30-client 
+#
+# systemctl enable containerd
+# systemctl start containerd
+# # Enable IP forwarding
+# echo "1" > /proc/sys/net/ipv4/ip_forward
+# # Make IP forwarding persistent across reboots
+# echo "net.ipv4.ip_forward = 1" > /etc/sysctl.d/99-kubernetes-cri.conf
+# sysctl --system
+#
+# # Initialize kubernetes
+# kubeadm init
+
+
+# Now helm should work
+
+
