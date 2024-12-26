321c3182ff449b7ef9215054577f31f77400f698
Nicole Venner <nvenner@protonmail.ch>
2024-12-26 14:03:17 -0700

    make an attempt at using cert-manager

--- k8s/install-k8s-initial.sh
+++ k8s/install-k8s-initial.sh
@@ -35,6 +35,14 @@ kubectl create clusterrolebinding nicole-admin-binding \
 # Create a token for the service account
 kubectl create token nicole
 
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+ cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --set installCRDs=true
+
 
 cd / 
 mkdir mycorrhiza
