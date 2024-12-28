1d9ab0cd84e3805dacf6a2df9edb65b160b6a51a
Nicole Venner <nvenner@protonmail.ch>
2024-12-28 14:00:18 -0700

    stashed k8s changes

--- k8s/install-k8s-initial.sh
+++ k8s/install-k8s-initial.sh
@@ -40,6 +40,10 @@ kubectl create clusterrolebinding nicole-admin-binding \
     --clusterrole=cluster-admin \
     --serviceaccount=default:nicole
 
+# helm repo add jetstack https://charts.jetstack.io
+# helm repo update
+# kubectl create namespace cert-manager
+# helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.9.1 --set installCRDs=true
 # Create a token for the service account
 # kubectl create token nicole
 
