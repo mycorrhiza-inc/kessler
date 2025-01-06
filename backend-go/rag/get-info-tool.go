d1a79240e0f58779ee38fe47893eb84dad534678
Nicole Venner <nvenner@protonmail.ch>
2025-01-06 10:50:52 -0700

    example information

--- backend-go/rag/get-info-tool.go
+++ backend-go/rag/get-info-tool.go
@@ -6,15 +6,37 @@ import (
 	"github.com/google/uuid"
 )
 
-func getObjectInformation(obj_uuid uuid.UUID, obj_type string, q dbstore.Queries) (map[string]interface{}, error) {
-	exampleInfo := map[string]interface{}{}
+type ObjectInfo struct {
+	UUID        uuid.UUID `json:"uuid"`
+	Name        string    `json:"name"`
+	ObjectType  string    `json:"object_type"`
+	Description string    `json:"description"`
+}
+
+func getObjectInformation(obj_uuid uuid.UUID, obj_type string, q dbstore.Queries) (ObjectInfo, error) {
+	exampleInfo := ObjectInfo{UUID: obj_uuid, ObjectType: obj_type, Name: "Example", Description: "Example Description"}
 	switch obj_type {
 	case "file":
-		return exampleInfo, nil
+		return getFileInformation(obj_uuid, q)
 	case "org":
-		return exampleInfo, nil
+		return getOrgInformation(obj_uuid, q)
 	case "docket":
-		return exampleInfo, nil
+		return getDocketInformation(obj_uuid, q)
 	}
+	return exampleInfo, nil
+}
+
+func getFileInformation(file_uuid uuid.UUID, q dbstore.Queries) (ObjectInfo, error) {
+	exampleInfo := ObjectInfo{UUID: file_uuid, ObjectType: "file", Name: "Example File", Description: "Example File Description"}
+	return exampleInfo, nil
+}
+
+func getOrgInformation(org_uuid uuid.UUID, q dbstore.Queries) (ObjectInfo, error) {
+	exampleInfo := ObjectInfo{UUID: org_uuid, ObjectType: "org", Name: "Example Organization", Description: "Example Organization Description"}
+	return exampleInfo, nil
+}
+
+func getDocketInformation(docket_uuid uuid.UUID, q dbstore.Queries) (ObjectInfo, error) {
+	exampleInfo := ObjectInfo{UUID: docket_uuid, ObjectType: "docket", Name: "Example Docket", Description: "Example Docket Description"}
 	return exampleInfo, nil
 }
