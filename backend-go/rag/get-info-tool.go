2c5944d0fd294b559995220001310feaf2438eb6
Nicole Venner <nvenner@protonmail.ch>
2025-01-06 17:40:09 -0700

    more info for various models

--- backend-go/rag/get-info-tool.go
+++ backend-go/rag/get-info-tool.go
@@ -1,34 +1,50 @@
 package rag
 
 import (
+	"context"
+	"fmt"
+	"kessler/crud"
 	"kessler/gen/dbstore"
 
 	"github.com/google/uuid"
 )
 
 type ObjectInfo struct {
-	UUID        uuid.UUID `json:"uuid"`
-	Name        string    `json:"name"`
-	ObjectType  string    `json:"object_type"`
-	Description string    `json:"description"`
+	UUID        uuid.UUID              `json:"uuid"`
+	Name        string                 `json:"name"`
+	ObjectType  string                 `json:"object_type"`
+	Description string                 `json:"description"`
+	Extras      map[string]interface{} `json:"extras"`
 }
 
-func getObjectInformation(obj_uuid uuid.UUID, obj_type string, q dbstore.Queries) (ObjectInfo, error) {
+func getObjectInformation(obj_uuid uuid.UUID, obj_type string, q dbstore.Queries, ctx context.Context) (ObjectInfo, error) {
 	exampleInfo := ObjectInfo{UUID: obj_uuid, ObjectType: obj_type, Name: "Example", Description: "Example Description"}
 	switch obj_type {
 	case "file":
-		return getFileInformation(obj_uuid, q)
+		return getFileInformation(obj_uuid, q, ctx)
 	case "org":
 		return getOrgInformation(obj_uuid, q)
 	case "docket":
 		return getDocketInformation(obj_uuid, q)
 	}
-	return exampleInfo, nil
+	return exampleInfo, fmt.Errorf("Object type %s did not match anything known", obj_type)
 }
 
-func getFileInformation(file_uuid uuid.UUID, q dbstore.Queries) (ObjectInfo, error) {
-	exampleInfo := ObjectInfo{UUID: file_uuid, ObjectType: "file", Name: "Example File", Description: "Example File Description"}
-	return exampleInfo, nil
+func getFileInformation(file_uuid uuid.UUID, q dbstore.Queries, ctx context.Context) (ObjectInfo, error) {
+	returnInfo := ObjectInfo{UUID: file_uuid, ObjectType: "file", Name: "Example File", Description: "Example File Description"}
+	file, err := crud.SemiCompleteFileGetFromUUID(ctx, q, file_uuid)
+	if err != nil {
+		return ObjectInfo{}, err
+	}
+	returnInfo.Name = file.Name
+	returnInfo.Description = file.Extra.Summary
+	returnInfo.Extras["date"] = file.Mdata["date"]
+	returnInfo.Extras["file_extension"] = file.Extension
+	returnInfo.Extras["parent_docket_name"] = file.Conversation.Name
+	returnInfo.Extras["parent_docket_goverment_id"] = file.Conversation.DocketID
+	returnInfo.Extras["parent_docket_uuid"] = file.Conversation.ID
+
+	return returnInfo, nil
 }
 
 func getOrgInformation(org_uuid uuid.UUID, q dbstore.Queries) (ObjectInfo, error) {
