5b061d05be3446a38623c7902fe27f9152bf284f
M Bright <mbright@mycor.io>
2025-01-12 13:54:38 -0700

    match absolute imports path in jest config

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -10,6 +10,10 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+  }
+
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
