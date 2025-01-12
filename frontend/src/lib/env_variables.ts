4313a59707908834e4eeda9fb6b83f222053d5fb
M Bright <mbright@mycor.io>
2025-01-12 13:46:38 -0700

    fix env vars to use process.env

--- frontend/src/lib/env_variables.ts
+++ frontend/src/lib/env_variables.ts
@@ -1,13 +1,9 @@
-import getConfig from "next/config";
-
-const { publicRuntimeConfig, serverRuntimeConfig } = getConfig();
-
 // This url is for hitting the kessler api from the client
 export const publicAPIURL =
-  publicRuntimeConfig.NEXT_PUBLIC_KESSLER_API_URL || "https://api.kessler.xyz";
+  process.env.NEXT_PUBLIC_KESSLER_API_URL || "https://api.kessler.xyz";
 export const isLocalMode = publicAPIURL.indexOf("localhost") !== -1;
 
 // This is the url for hitting the api, for internal use by the nextjs server runtime
 export const internalAPIURL =
-  serverRuntimeConfig.INTERNAL_KESSLER_API_URL || "https://api.kessler.xyz";
+  process.env.INTERNAL_KESSLER_API_URL || "https://api.kessler.xyz";
 // This url is for hitting the kessler api fVrom the client,
