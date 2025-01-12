6cd35ada5e9ed7d41e9a483087987a9a260cfd40
Nicole Venner <nvenner@protonmail.ch>
2025-01-12 08:23:44 -0700

    delete local .env files for k8s support

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    KESSLER_API_URL: process.env.KESSLER_API_URL,
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    INTERNAL_KESSLER_API_URL: process.env.INTERNAL_KESSLER_API_URL,
+  },
+};
 
 module.exports = nextConfig;
--- frontend/src/lib/env_variables.ts
+++ frontend/src/lib/env_variables.ts
@@ -1,14 +1,13 @@
-// This url is for hitting the kessler api from the client,
-export const publicAPIURL =
-  process.env.NEXT_PUBLIC_KESSLER_API_URL || "https://api.kessler.xyz";
-export const isLocalMode = publicAPIURL.indexOf("localhost") !== -1;
-// This is the url for hitting the api, for internal use by the nextjs server runtime, primarially for SSR pages.
-// This defaults to the prod url for now, since setting it to localhost doesnt work because the api isnt accessible from the localhost of
-// the nextjs docker container.
-// TODO: Figure out why replacing this with http://backend-go:4041 doesnt work.
-// which would improve security and prevent a network route outside the system for SSR.
-// It does work for docker compose, k8s hates it though so it defaults to the api rn.
-export const internalAPIURL =
-  process.env.INTERNAL_KESSLER_API_URL || "https://api.kessler.xyz";
+import getConfig from "next/config";
 
-// export const internalAPIURL = "https://api.kessler.xyz";
+const { publicRuntimeConfig, serverRuntimeConfig } = getConfig();
+
+// This url is for hitting the kessler api from the client
+export const publicAPIURL =
+  publicRuntimeConfig.NEXT_PUBLIC_KESSLER_API_URL || "https://api.kessler.xyz";
+export const isLocalMode = publicAPIURL.indexOf("localhost") !== -1;
+
+// This is the url for hitting the api, for internal use by the nextjs server runtime
+export const internalAPIURL =
+  serverRuntimeConfig.INTERNAL_KESSLER_API_URL || "https://api.kessler.xyz";
+// This url is for hitting the kessler api fVrom the client,
