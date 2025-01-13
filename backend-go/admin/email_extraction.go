e58b2efadb6c117e643258fff0f41327bb446d2e
Nicole Venner <nvenner@protonmail.ch>
2025-01-12 18:24:23 -0700

    finished initial extract email code

--- backend-go/admin/email_extraction.go
+++ backend-go/admin/email_extraction.go
@@ -2,7 +2,9 @@ package admin
 
 import (
 	"context"
+	"fmt"
 	"kessler/crud"
+	"kessler/gen/dbstore"
 	"kessler/objects/files"
 	"kessler/rag"
 	"kessler/routing"
@@ -48,9 +50,7 @@ Return only the email addresses that are likely associated with the organization
 	return relavent_emails, nil
 }
 
-func extractRelaventEmailsFromFileUUID(llm rag.LLMModel, file_id uuid.UUID) ([]string, error) {
-	ctx := context.Background()
-	q := *routing.DBQueriesFromContext(ctx)
+func extractRelaventEmailsFromFileUUID(ctx context.Context, q dbstore.Queries, llm rag.LLMModel, file_id uuid.UUID) ([]string, error) {
 	file_object, err := crud.CompleteFileSchemaGetFromUUID(ctx, q, file_id)
 	if err != nil {
 		return []string{}, err
@@ -67,4 +67,55 @@ func extractRelaventEmailsFromFileUUID(llm rag.LLMModel, file_id uuid.UUID) ([]s
 		return emails, err
 	}
 	return emails, nil
+}
+
+type EmailOrgExtraction struct {
+	OrganizationName string      `json:"organization_name"`
+	OrganizationUUID uuid.UUID   `json:"organization_uuid"`
+	FilesCount       int         `json:"files_count"`
+	EmailInfo        []EmailInfo `json:"email_info"`
+}
+
+type EmailInfo struct {
+	Email           string      `json:"email"`
+	AssociatedFiles []uuid.UUID `json:"associated_files"`
+}
+
+func extractRelaventEmailsFromOrgUUID(ctx context.Context, llm rag.LLMModel, org_id uuid.UUID) (EmailOrgExtraction, error) {
+	q := *routing.DBQueriesFromContext(ctx)
+	information_map := map[string][]uuid.UUID{}
+	org_obj, err := crud.OrgWithFilesGetByID(ctx, &q, org_id)
+	if err != nil {
+		return EmailOrgExtraction{}, err
+	}
+	file_obj_list := org_obj.FilesAuthored
+	for _, file_obj := range file_obj_list {
+		file_id := file_obj.ID
+		emails, err := extractRelaventEmailsFromFileUUID(ctx, q, llm, file_id)
+		if err != nil {
+			fmt.Printf("Encountered error getting emails from file with uuid: %v", file_id)
+		}
+		if err == nil {
+			for _, email := range emails {
+				results, ok := information_map[email]
+				if ok {
+					information_map[email] = append(results, file_id)
+				} else {
+					information_map[email] = []uuid.UUID{file_id}
+				}
+			}
+		}
+	}
+	email_infos := []EmailInfo{}
+	for key, value := range information_map {
+		email_info := EmailInfo{Email: key, AssociatedFiles: value}
+		email_infos = append(email_infos, email_info)
+	}
+	return_info := EmailOrgExtraction{
+		OrganizationName: org_obj.Name,
+		OrganizationUUID: org_obj.ID,
+		FilesCount:       len(org_obj.FilesAuthored),
+		EmailInfo:        email_infos,
+	}
+	return return_info, nil
 }
