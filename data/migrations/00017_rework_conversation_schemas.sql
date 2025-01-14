a8dbf5aa5224bc18be04574df03d17d7d447782a
Nicole Venner <nvenner@protonmail.ch>
2025-01-14 14:17:46 -0700

    rename all the stuff in conversations.sql

--- data/migrations/00017_rework_conversation_schemas.sql
+++ data/migrations/00017_rework_conversation_schemas.sql
@@ -20,8 +20,15 @@ ALTER TABLE
     public.docket_conversations
 RENAME COLUMN docket_id TO docket_gov_id;
 
+ALTER TABLE
+    public.docket_documents
+RENAME COLUMN docket_id TO conversation_uuid;
 
 -- +goose Down
+
+ALTER TABLE
+    public.docket_documents
+RENAME COLUMN conversation_uuid TO docket_id;
 
 ALTER TABLE
     public.docket_conversations
