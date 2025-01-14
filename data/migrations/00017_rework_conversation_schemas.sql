b091d473dc9f8be8805cda2ca426b0a03c82b5e4
Nicole Venner <nvenner@protonmail.ch>
2025-01-14 14:08:38 -0700

    add new migration code

---
+++ data/migrations/00017_rework_conversation_schemas.sql
@@ -0,0 +1,40 @@
+-- +goose Up
+
+ALTER TABLE
+    public.docket_conversations
+ADD
+    COLUMN matter_type VARCHAR NOT NULL DEFAULT "";
+
+ALTER TABLE
+    public.docket_conversations
+ADD
+    COLUMN matter_subtype VARCHAR NOT NULL DEFAULT "";
+
+ALTER TABLE
+    public.docket_conversations
+ADD
+    COLUMN industry_type VARCHAR NOT NULL DEFAULT "";
+
+
+ALTER TABLE
+    public.docket_conversations
+RENAME COLUMN docket_id TO docket_gov_id;
+
+
+-- +goose Down
+
+ALTER TABLE
+    public.docket_conversations
+RENAME COLUMN docket_gov_id TO docket_id;
+
+ALTER TABLE
+    public.docket_conversations
+DROP COLUMN industry_type;
+
+ALTER TABLE
+    public.docket_conversations
+DROP COLUMN matter_subtype;
+
+ALTER TABLE
+    public.docket_conversations
+DROP COLUMN matter_type;
