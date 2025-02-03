ea336e5937358e39789363a7a0edc7ab89786ea8
Nicole Venner <nvenner@protonmail.ch>
2025-02-03 07:00:50 -0700

    llm bungled the sql query

--- data/queries/jobs.sql
+++ data/queries/jobs.sql
@@ -1,23 +1,23 @@
--- name: CreateJob :one 
+-- name: CreateJob :one
 INSERT INTO
-	public.jobs (
-		id,
-		created_at,
-		updated_at,
-		priority,
-		name,
-		status,
-		job_type,
-		payload
-	)
+    public.jobs (
+        id,
+        job_priority,
+        job_name,
+        job_status,
+        job_type,
+        job_data,
+        created_at,
+        updated_at
+    )
 VALUES
-	($1, $2, $3, $4, $5, $6, $7, NOW(), NOW())
+    ($1, $2, $3, $4, $5, $6, NOW(), NOW())
 RETURNING
-	id;	
+    id;
+
 
 -- name: DeleteJob :exec
 DELETE FROM
-	public.jobs
+    public.jobs
 WHERE
-	id = $1;
-	
+    id = $1;
