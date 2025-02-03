f590aac2850e412f59b39234116add0cfefff360
Nicole Venner <nvenner@protonmail.ch>
2025-02-03 16:28:59 -0700

    fix misnamed docker file stuff

--- update-docker-compose.sh
+++ update-docker-compose.sh
@@ -69,10 +69,10 @@ function process_branch() {
         sudo docker push "fractalhuman1/kessler-backend-go:${current_hash}"
 
         # Update docker-compose.yml on the server
-        ssh root@kessler.xyz "sed -i \"s|image: fractalhuman1/kessler-frontend:.*|image: fractalhuman1/kessler-frontend:${current_hash}|\" /mycorrhiza/kessler/docker-compose.deploy.yml && \
-        sed -i \"s|image: fractalhuman1/kessler-backend-go:.*|image: fractalhuman1/kessler-backend-go:${current_hash}|\" /mycorrhiza/kessler/docker-compose.deploy.yml && \
-        sed -i \"s|CONFIG_HASH:.*|CONFIG_HASH: ${current_hash}|\" /mycorrhiza/kessler/docker-compose.deploy.yml && \
-        cd /mycorrhiza/kessler && git clean -fd && git pull && docker compose down && docker compose up -d"
+        ssh root@kessler.xyz "sed -i \"s|image: fractalhuman1/kessler-frontend:.*|image: fractalhuman1/kessler-frontend:${current_hash}|\" /mycorrhiza/kessler/docker-compose.deploy.yaml && \
+        sed -i \"s|image: fractalhuman1/kessler-backend-go:.*|image: fractalhuman1/kessler-backend-go:${current_hash}|\" /mycorrhiza/kessler/docker-compose.deploy.yaml && \
+        sed -i \"s|CONFIG_HASH:.*|CONFIG_HASH: ${current_hash}|\" /mycorrhiza/kessler/docker-compose.deploy.yaml && \
+        cd /mycorrhiza/kessler && git clean -fd && git pull && docker compose -f docker-compose.deploy.yaml down && docker compose -f docker-compose.deploy.yaml up -d"
     else
         echo "No changes detected, deployemnt already on provided hash, skipping deployment: ${current_hash}"
     fi
