4b796748def52b1464b2a635fdb37941c7483850
Nicole Venner <nvenner@protonmail.ch>
2025-02-04 20:11:33 -0700

    big refactor for quickwit file ingest

--- backend-go/jobs/quickwit_files_ingest.go
+++ backend-go/jobs/quickwit_files_ingest.go
@@ -2,14 +2,18 @@ package jobs
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"kessler/admin"
 	"kessler/gen/dbstore"
+	"kessler/objects/authors"
+	"kessler/objects/conversations"
+	"kessler/objects/files"
+	"kessler/objects/timestamp"
 	"kessler/quickwit"
 	"kessler/util"
 	"net/http"
 
-	"github.com/google/uuid"
 	"golang.org/x/exp/rand"
 )
 
@@ -32,36 +36,71 @@ func HandleQuickwitFileIngestFromPostgres(w http.ResponseWriter, r *http.Request
 	w.Write([]byte("Sucessfully ingested from postgres"))
 }
 
+func ParseQuickwitFileIntoCompleteSchema(file_raw dbstore.SemiCompleteFileQuickwitListGetRow) (files.CompleteFileSchema, error) {
+	var mdata_obj files.FileMetadataSchema
+	err := json.Unmarshal(file_raw.Mdata, &mdata_obj)
+	if err != nil {
+		return files.CompleteFileSchema{}, err
+	}
+	author_list := make([]authors.AuthorInformation, len(file_raw.OrganizationIds))
+	for i := range file_raw.OrganizationIds {
+		author_list[i] = authors.AuthorInformation{
+			AuthorName: file_raw.OrganizationNames[i],
+			AuthorID:   file_raw.OrganizationIds[i],
+		}
+	}
+	text_list := make([]files.FileChildTextSource, len(file_raw.FileTexts))
+	for i := range file_raw.FileTexts {
+		text_list[i] = files.FileChildTextSource{
+			IsOriginalText: false,
+			Text:           file_raw.FileTexts[i],
+			Language:       file_raw.FileTextLanguages[i],
+		}
+	}
+
+	file := files.CompleteFileSchema{
+		ID:            file_raw.ID,
+		Verified:      file_raw.Verified.Bool,
+		Extension:     file_raw.Extension,
+		Lang:          "en",
+		Name:          file_raw.Name,
+		Hash:          file_raw.Hash,
+		IsPrivate:     false,
+		DatePublished: timestamp.KesslerTime(file_raw.DatePublished.Time),
+		Mdata:         mdata_obj,
+		Stage:         files.DocProcStage{},
+		Extra:         files.FileGeneratedExtras{},
+		Authors:       author_list,
+		Conversation: conversations.ConversationInformation{
+			DocketGovID: file_raw.DocketGovID.String,
+			ID:          file_raw.ConversationUuid.Bytes,
+		},
+		DocTexts: text_list,
+	}
+	return file, nil
+}
+
 func QuickwitIngestFromPostgres(q *dbstore.Queries, ctx context.Context, filter_out_unverified bool) error {
 	indexName := quickwit.NYPUCIndexName
 	var files []dbstore.File
 	var err error
 
-	files, err = q.SemiCompleteFileQuickwitListGet(ctx)
+	files_raw, err := q.SemiCompleteFileQuickwitListGet(ctx)
 	if err != nil {
 		return err
 	}
 
 	if filter_out_unverified {
 		fmt.Printf("Got raw n files from postgres: %d\n", len(files))
-		var new_files []dbstore.File
+		var new_raw_files []dbstore.SemiCompleteFileQuickwitListGetRow
 
-		for _, file := range files {
+		for _, file := range files_raw {
 			if file.Verified.Bool {
-				verifiedFiles = append(verifiedFiles, file)
+				new_raw_files = append(new_raw_files, file)
 			}
 		}
-		files = verifiedFiles
+		files_raw = new_raw_files
 
-	} else {
-		files, err = q.FilesList(ctx)
-		if err != nil {
-			return err
-		}
-	}
-	ids := make([]uuid.UUID, len(files))
-	for i, file := range files {
-		ids[i] = file.ID
 	}
 	// Randomize the uuids so that you dont have weird unexpected behavior near the beginning or end.
 	for index := range ids {
