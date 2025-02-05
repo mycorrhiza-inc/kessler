a028da7a278f01772065386ab2df70b6c575211c
Nicole Venner <nvenner@protonmail.ch>
2025-02-05 11:41:01 -0700

    add incremental offset file ingest script

--- data/queries/quickwit_ingest.sql
+++ data/queries/quickwit_ingest.sql
@@ -61,6 +61,70 @@ GROUP BY
     docket_documents.conversation_uuid,
     docket_conversations.docket_gov_id;
 
+-- name: SemiCompleteFileQuickwitListGetPaginated :many
+SELECT
+    public.file.id,
+    public.file.name,
+    public.file.extension,
+    public.file.lang,
+    public.file.verified,
+    public.file.hash,
+    public.file.created_at,
+    public.file.updated_at,
+    public.file.date_published,
+    public.file_metadata.mdata,
+    public.file_extras.extra_obj,
+    public.docket_documents.conversation_uuid,
+    public.docket_conversations.docket_gov_id,
+    array_agg(
+        public.organization.id
+        ORDER BY
+            public.organization.id
+    ) :: uuid [] AS organization_ids,
+    array_agg(
+        public.organization.name
+        ORDER BY
+            public.organization.id
+    ) :: text [] AS organization_names,
+    array_agg(
+        public.organization.is_person
+        ORDER BY
+            public.organization.id
+    ) :: boolean [] AS is_person_list,
+    array_agg(
+        public.file_text_source.text
+        ORDER BY
+            public.file_text_source.id
+    ) :: VARCHAR [] AS file_texts,
+    array_agg(
+        public.file_text_source.language
+        ORDER BY
+            public.file_text_source.id
+    ) :: VARCHAR [] AS file_text_languages
+FROM
+    public.file
+    LEFT JOIN public.file_metadata ON public.file.id = public.file_metadata.id
+    LEFT JOIN public.file_extras ON public.file.id = public.file_extras.id
+    LEFT JOIN public.docket_documents ON public.file.id = public.docket_documents.file_id
+    LEFT JOIN public.docket_conversations ON public.docket_documents.conversation_uuid = public.docket_conversations.id
+    LEFT JOIN public.relation_documents_organizations_authorship ON public.file.id = public.relation_documents_organizations_authorship.document_id
+    LEFT JOIN public.organization ON public.relation_documents_organizations_authorship.organization_id = public.organization.id
+GROUP BY
+    FILE.id,
+    FILE.name,
+    FILE.extension,
+    FILE.lang,
+    FILE.verified,
+    FILE.hash,
+    FILE.created_at,
+    FILE.updated_at,
+    FILE.date_published,
+    file_metadata.mdata,
+    file_extras.extra_obj,
+    docket_documents.conversation_uuid,
+    docket_conversations.docket_gov_id
+LIMIT
+    $1 OFFSET $2;
 -- name: OrganizationCompleteQuickwitListGet :many
 SELECT
     public.organization.id,
