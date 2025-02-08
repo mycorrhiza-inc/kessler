29134408fcd6153ead1dad1933c39d901d82d36f
Nicole Venner <nvenner@protonmail.ch>
2025-02-07 19:39:07 -0700

    run sql migration for computed views

--- data/migrations/00021_create_materialized_view_author.sql
+++ data/migrations/00021_create_materialized_view_author.sql
@@ -1,22 +1,40 @@
 -- +goose Up
 CREATE MATERIALIZED VIEW testmat AS
 SELECT 
-	public.file.id,
-	JSONB_AGG(
-		JSONB_BUILD_OBJECT(
-			'id',
-			public.organization.id,
-			'name',
-			public.organization.name,
-			'is_person',
-			public.organization.is_person
-		)
-	) FILTER (WHERE public.organization.id IS NOT NULL) AS organizations
-FROM public.file
-	LEFT JOIN public.relation_documents_organizations_authorship 
-		ON public.file.id = public.relation_documents_organizations_authorship.document_id
-	LEFT JOIN public.organization 
-		ON public.relation_documents_organizations_authorship.organization_id = public.organization.id
-GROUP BY public.file.id;
+    f.id,
+    f.name,
+    f.extension,
+    f.lang,
+    f.verified,
+    f.hash,
+    f.created_at,
+    f.updated_at,
+    f.date_published,
+    fm.mdata,
+    fe.extra_obj,
+    dd.conversation_uuid,
+    dc.docket_gov_id,
+    COALESCE(fts.text, '') AS file_text,
+    JSONB_AGG(
+        JSONB_BUILD_OBJECT(
+            'id', o.id,
+            'name', o.name,
+            'is_person', o.is_person
+        )
+    ) FILTER (WHERE o.id IS NOT NULL) AS organizations
+FROM public.file AS f
+  LEFT JOIN public.file_metadata fm ON f.id = fm.id
+  LEFT JOIN public.file_extras fe ON f.id = fe.id
+  LEFT JOIN (
+      SELECT DISTINCT ON (file_id) file_id, text
+      FROM public.file_text_source
+      WHERE language = 'en'
+      ORDER BY file_id, id
+  ) fts ON f.id = fts.file_id
+  LEFT JOIN public.docket_documents dd ON f.id = dd.file_id
+  LEFT JOIN public.docket_conversations dc ON dd.conversation_uuid = dc.id
+  LEFT JOIN public.relation_documents_organizations_authorship rdoa ON f.id = rdoa.document_id
+  LEFT JOIN public.organization o ON rdoa.organization_id = o.id
+GROUP BY f.id;
 -- +goose Down
 DROP MATERIALIZED VIEW IF EXISTS testmat;
