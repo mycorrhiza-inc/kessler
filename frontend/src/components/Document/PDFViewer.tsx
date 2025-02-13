ade4389427dbdc1a046d4836f29a05d49bd9c322
Nicole Venner <nvenner@protonmail.ch>
2025-02-13 07:46:32 -0700

    seperate out pdf downloading into our code for better error handling

--- frontend/src/components/Document/PDFViewer.tsx
+++ frontend/src/components/Document/PDFViewer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useResizeObserver } from "@wojtekmaj/react-hooks";
 import { pdfjs, Document, Page } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -10,6 +10,9 @@ import dynamic from "next/dynamic";
 import "./PDFViewer.css";
 
 import type { PDFDocumentProxy } from "pdfjs-dist";
+import LoadingSpinner from "../styled-components/LoadingSpinner";
+import ErrorMessage from "../ErrorMessage";
+import { fi } from "date-fns/locale";
 
 // TODO : Inline at some point so we dont get screwed by a malicious cdn.
 pdfjs.GlobalWorkerOptions.workerSrc =
@@ -27,7 +30,61 @@ const maxWidth = 800;
 
 type PDFFile = string | File | null;
 
-function PDFViewer({
+const PDFViewer = ({ file }: { file: string }) => {
+  const [downloading, setDownloading] = useState(true);
+  const [err, setErr] = useState("");
+  const [fileData, setFileData] = useState<File>();
+
+  const getPDFData = async (url: string): Promise<File> => {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(
+        `Server Returned Bad Error Code: ${response.status} ${response.statusText}`,
+      );
+    }
+    const blob = await response.blob();
+    return new File([blob], "document.pdf", { type: "application/pdf" });
+  };
+  const getPDFDataHandleErrors = async (url: string): Promise<File> => {
+    setDownloading(true);
+    try {
+      const result = await getPDFData(url);
+      setDownloading(false);
+      return result;
+    } catch (error) {
+      setDownloading(false);
+      const errString = `Failed to fetch PDF: ${error}`;
+      setErr(errString);
+      throw new Error(errString);
+    }
+  };
+  useEffect(() => {
+    let active = true;
+    load();
+    return () => {
+      active = false;
+    };
+    async function load() {
+      const res = await getPDFDataHandleErrors(file);
+      if (!active) {
+        return;
+      }
+      setFileData(res);
+    }
+  }, [file]);
+  if (downloading) {
+    return <LoadingSpinner loadingText="Loading PDF Data" />;
+  }
+  if (err) {
+    return <ErrorMessage error={err} />;
+  }
+  if (fileData === undefined) {
+    return <ErrorMessage error="PDF Data is Undefined" />;
+  }
+  return <PDFViewerRaw file={fileData} setLoading={() => {}} />;
+};
+
+function PDFViewerRaw({
   file,
   setLoading,
 }: {
