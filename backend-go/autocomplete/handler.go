6c303242bfe94df67e3e652f546c26d609925de4
Nicole Venner <nvenner@protonmail.ch>
2025-02-13 20:12:42 -0700

    write backend networking for query

--- backend-go/autocomplete/handler.go
+++ backend-go/autocomplete/handler.go
@@ -1,8 +1,11 @@
 package autocomplete
 
 import (
+	"context"
+	"kessler/quickwit"
 	"net/http"
 
+	"github.com/charmbracelet/log"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
@@ -24,5 +27,73 @@ type AutoCompleteHit struct {
 	Type string    `json:"type"`
 }
 
-func AutoCompleteFileGetResults(query string) []AutoCompleteHit {
+func AutoCompleteFileGetResults(query string, ctx context.Context) ([]AutoCompleteHit, error) {
+	results_each := 10
+	type AsyncResult struct {
+		Results []AutoCompleteHit
+		Err     error
+	}
+
+	convoChan := make(chan AsyncResult, 1)
+	orgChan := make(chan AsyncResult, 1)
+	go func() {
+		search_convo_results := quickwit.ConvoSearchRequestData{
+			Search: quickwit.ConversationSearchSchema{
+				Query: query,
+			},
+			Limit:  results_each,
+			Offset: 0,
+		}
+		results, err := quickwit.SearchConversations(search_convo_results, ctx)
+		if err != nil {
+			log.Error("Encountered Error while getting quickwit autocomplete", "err", err)
+			convoChan <- AsyncResult{Err: err}
+			return
+		}
+		autocomplete_hits := make([]AutoCompleteHit, 0, len(results))
+		for index, result := range results {
+			autocomplete_hits[index] = AutoCompleteHit{
+				ID:   result.ID,
+				Name: result.Name,
+				Type: "conversation",
+			}
+		}
+		convoChan <- AsyncResult{Results: autocomplete_hits}
+		return
+	}()
+
+	go func() {
+		search_convo_results := quickwit.OrgSearchRequestData{
+			Search: quickwit.OrganizationSearchSchema{
+				Query: query,
+			},
+			Limit:  results_each,
+			Offset: 0,
+		}
+		results, err := quickwit.SearchOrganizations(search_convo_results, ctx)
+		if err != nil {
+			log.Error("Encountered Error while getting quickwit autocomplete", "err", err)
+			orgChan <- AsyncResult{Err: err}
+			return
+		}
+		autocomplete_hits := make([]AutoCompleteHit, 0, len(results))
+		for index, result := range results {
+			autocomplete_hits[index] = AutoCompleteHit{
+				ID:   result.ID,
+				Name: result.Name,
+				Type: "organization",
+			}
+		}
+		orgChan <- AsyncResult{Results: autocomplete_hits}
+		return
+	}()
+	orgResults := <-orgChan
+	if orgResults.Err != nil {
+		return []AutoCompleteHit{}, nil
+	}
+	convoResults := <-convoChan
+	if convoResults.Err != nil {
+		return []AutoCompleteHit{}, nil
+	}
+	return append(orgResults.Results, convoResults.Results...), nil
 }
