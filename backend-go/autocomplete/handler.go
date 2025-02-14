6f0c0bebb75cf1f79595858f66e25f45bff7e2b6
Nicole Venner <nvenner@protonmail.ch>
2025-02-13 19:45:15 -0700

    initial autocomplete stuff

--- backend-go/autocomplete/handler.go
+++ backend-go/autocomplete/handler.go
@@ -3,16 +3,26 @@ package autocomplete
 import (
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
 func DefineAutocompleteRoutes(autocomplete_subrouter *mux.Router) {
 	autocomplete_subrouter.HandleFunc(
-		"/conversation-autocomplete",
-		AutocompleteConversationHandler,
+		"/files-autocomplete",
+		AutocompleteFileHandler,
 	).Methods(http.MethodGet)
 }
 
-func AutocompleteConversationHandler(w http.ResponseWriter, r *http.Request) {
+func AutocompleteFileHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world"))
+}
+
+type AutoCompleteHit struct {
+	ID   uuid.UUID `json:"uuid"`
+	Name string    `json:"name"`
+	Type string    `json:"type"`
+}
+
+func AutoCompleteFileGetResults(query string) []AutoCompleteHit {
 }
