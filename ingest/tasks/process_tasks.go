061a0a5a4221c3a4ddf5c600c19fec25856efa0e
Nicole Venner <nvenner@protonmail.ch>
2025-02-27 18:30:42 -0700

    actually fucking add the asynq handler

---
+++ ingest/tasks/process_tasks.go
@@ -0,0 +1,33 @@
+package tasks
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"thaumaturgy/common/objects/files"
+	"thaumaturgy/logic"
+
+	"github.com/charmbracelet/log"
+	"github.com/hibiken/asynq"
+)
+
+func AsynqHandler(mux *asynq.ServeMux) {
+	mux.HandleFunc("ingest:file", HandleIngestNewFileTask)
+}
+
+func HandleIngestNewFileTask(ctx context.Context, task *asynq.Task) error {
+	var fileObj files.CompleteFileSchema
+	if err := json.Unmarshal(task.Payload(), &fileObj); err != nil {
+		return err
+	}
+	file_URL, ok := fileObj.Mdata["url"].(string)
+	if !ok {
+		return fmt.Errorf("url field missing or not a string in file metadata")
+	}
+	file, err := logic.AddURLRaw(ctx, file_URL, &fileObj)
+	if err != nil {
+		return err
+	}
+	log.Info("File added to DB", "file", file)
+	return nil
+}
