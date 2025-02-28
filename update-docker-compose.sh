7a535bbb8e445124cb57e4010b457c9897a94ca9
Nicole Venner <nvenner@protonmail.ch>
2025-02-27 18:58:53 -0700

    add ingest to production deployment server

--- update-docker-compose.sh
+++ update-docker-compose.sh
@@ -64,9 +64,11 @@ function process_branch() {
         # Build and push Docker images
         sudo docker build -t "fractalhuman1/kessler-frontend:${current_hash}" --platform linux/amd64 ./frontend/
         sudo docker build -t "fractalhuman1/kessler-backend-go:${current_hash}" --platform linux/amd64 ./backend-go/
+        sudo docker build -t "fractalhuman1/kessler-ingest:${current_hash}" --platform linux/amd64 ./ingest/
 
         sudo docker push "fractalhuman1/kessler-frontend:${current_hash}"
         sudo docker push "fractalhuman1/kessler-backend-go:${current_hash}"
+        sudo docker push "fractalhuman1/kessler-ingest:${current_hash}"
 
         # Update docker-compose.yml on the server
         # Set deployment variables based on environment
