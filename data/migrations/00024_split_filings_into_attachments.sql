fd78750718c17a131a3c8a7226f0672630996402
Nicole Venner <nvenner@protonmail.ch>
2025-03-08 10:37:47 -0700

    emergency not null constraints

--- data/migrations/00024_split_filings_into_attachments.sql
+++ data/migrations/00024_split_filings_into_attachments.sql
@@ -3,12 +3,12 @@
 CREATE TABLE IF NOT EXISTS public.attachment (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     file_id UUID REFERENCES public.file(id),  -- Changed from PRIMARY KEY to UNIQUE
-    lang VARCHAR,
-    name VARCHAR,
-    extension VARCHAR,
-    hash VARCHAR,
+    lang VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    extension VARCHAR NOT NULL,
+    hash VARCHAR NOT NULL,
     mdata JSONB,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
@@ -18,15 +18,15 @@ CREATE TABLE IF NOT EXISTS public.attachment_text_source (
     attachment_id UUID REFERENCES public.attachment(id) ON DELETE CASCADE,
     is_original_text BOOLEAN NOT NULL,
     language VARCHAR NOT NULL,
-    text TEXT,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
+    text TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS public.attachment_extras (
     id UUID PRIMARY KEY REFERENCES public.attachment(id),
-    extra_obj JSONB,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
+    extra_obj JSONB ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
