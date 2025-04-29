78981ba54ba4c7701b99c462de0ae349f0dc08b8
Nicole Venner <nvenner@protonmail.ch>
2025-04-29 08:48:47 -0600

    swap out hardcoded svg for react icon

--- frontend/src/components/NewSearch/SearchBox.tsx
+++ frontend/src/components/NewSearch/SearchBox.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { FaSearch } from "react-icons/fa";
 import React from "react";
 
 export default function SearchBox({
@@ -20,20 +21,7 @@ export default function SearchBox({
         value={value}
         onChange={(e) => onChange(e.target.value)}
       />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 text-gray-400 absolute right-3 top-1/2 transform -translate-y-1/2"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-        />
-      </svg>
+      <FaSearch className="h-6 w-6 text-gray-400 absolute right-3 top-1/2 transform -translate-y-1/2" />
     </div>
   );
 }
