d4e24bed2d478434fa5cbce51a85eff1fb5d099c
Nicole Venner <nvenner@protonmail.ch>
2025-05-02 22:13:34 -0600

    add command k search to markdown rendering space

--- frontend/src/app/(application)/admin-testing/markdown-test/page.tsx
+++ frontend/src/app/(application)/admin-testing/markdown-test/page.tsx
@@ -2,14 +2,14 @@
 import MarkdownRenderer, {
   testMarkdownContent,
 } from "@/components/MarkdownRenderer";
+import { CommandKSearch } from "@/components/Search/CommandK";
 import { ForceUseClient } from "@/components/UtilityComponents";
 export const dynamic = "force-dynamic";
 export default function Page() {
   return (
     <>
-        <ForceUseClient>
-          <MarkdownRenderer>{testMarkdownContent}</MarkdownRenderer>
-        </ForceUseClient>
+      <MarkdownRenderer>{testMarkdownContent}</MarkdownRenderer>
+      <CommandKSearch />
     </>
   );
 }
