d5371b7e49147d82cdb3ddb389e5cb465c137bd1
Nicole Venner <nvenner@protonmail.ch>
2025-05-06 16:18:38 -0600

    fmt file and fix bug

--- backend/internal/ingest/tasks/process_tasks.go
+++ backend/internal/ingest/tasks/process_tasks.go
@@ -1,47 +1,47 @@
 package tasks
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
+	"context"
+	"encoding/json"
+	"fmt"
+	"kessler/internal/ingest/logic"
+	"kessler/internal/objects/files"
 
-    "github.com/charmbracelet/log"
-    "github.com/hibiken/asynq"
-    "kessler/internal/ingest/logic"
-    "kessler/internal/objects/files"
+	"github.com/charmbracelet/log"
+	"github.com/hibiken/asynq"
 )
 
 func AsynqHandler(mux *asynq.ServeMux) {
-    // existing file ingestion
-    mux.HandleFunc("ingest:file", HandleIngestNewFileTask)
-    // new case ingestion
-    mux.HandleFunc(TypeIngestCase, HandleIngestCaseTask)
+	// existing file ingestion
+	mux.HandleFunc("ingest:file", HandleIngestNewFileTask)
+	// new case ingestion
+	mux.HandleFunc(TypeIngestCase, HandleIngestCaseTask)
 }
 
 func HandleIngestNewFileTask(ctx context.Context, task *asynq.Task) error {
-    var fileObj files.CompleteFileSchema
-    if err := json.Unmarshal(task.Payload(), &fileObj); err != nil {
-        return err
-    }
-    file, err := logic.CompleteIngestFileFromAttachmentUrls(ctx, &fileObj)
-    if err != nil {
-        return err
-    }
-    log.Info("File added to DB", "file", file)
-    return nil
+	var fileObj files.CompleteFileSchema
+	if err := json.Unmarshal(task.Payload(), &fileObj); err != nil {
+		return err
+	}
+	file, err := logic.CompleteIngestFileFromAttachmentUrls(ctx, &fileObj)
+	if err != nil {
+		return err
+	}
+	log.Info("File added to DB", "file", file)
+	return nil
 }
 
 // HandleIngestCaseTask processes a case ingestion task
 func HandleIngestCaseTask(ctx context.Context, task *asynq.Task) error {
-    var caseInfo CaseInfoPayload
-    if err := json.Unmarshal(task.Payload(), &caseInfo); err != nil {
-        return fmt.Errorf("failed to unmarshal case payload: %w", err)
-    }
-    // invoke business logic to persist case and filings
-    err := logic.IngestCase(ctx, &caseInfo)
-    if err != nil {
-        return fmt.Errorf("error ingesting case: %w", err)
-    }
-    log.Info("Case ingested successfully", "case_number", caseInfo.CaseNumber)
-    return nil
+	var caseInfo CaseInfoPayload
+	if err := json.Unmarshal(task.Payload(), &caseInfo); err != nil {
+		return fmt.Errorf("failed to unmarshal case payload: %w", err)
+	}
+	// invoke business logic to persist case and filings
+	err := IngestCase(ctx, &caseInfo)
+	if err != nil {
+		return fmt.Errorf("error ingesting case: %w", err)
+	}
+	log.Info("Case ingested successfully", "case_number", caseInfo.CaseNumber)
+	return nil
 }
