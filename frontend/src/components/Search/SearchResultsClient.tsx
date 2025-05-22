6e322e9ab488a67ddccdd9bdef8c9b6f86fa3cdd
Nicole Venner <nvenner@protonmail.ch>
2025-05-22 15:53:18 -0600

    move infinite scroll

--- frontend/src/components/Search/SearchResultsClient.tsx
+++ frontend/src/components/Search/SearchResultsClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useCallback, useEffect, useState } from "react";
-import InfiniteScrollPlus from "../InfiniteScroll/InfiniteScroll";
+import InfiniteScrollPlus from "../InfiniteScroll/InfiniteScrollPlus ";
 import RawSearchResults from "./RawSearchResults";
 import { useInfiniteSearch } from "@/lib/hooks/useInfiniteSearch";
 import {
