75bd1fac21d06ba251b577cc619c2bfb85a63778
Nicole Venner <nvenner@protonmail.ch>
2025-06-19 07:22:58 -0600

    handle error messages for errors in card rendering

--- frontend/src/components/stateful/RenderedObjectCards/RednderedObjectCard.tsx
+++ frontend/src/components/stateful/RenderedObjectCards/RednderedObjectCard.tsx
@@ -1,21 +1,27 @@
 import Card, { CardSize } from "@/components/style/cards/GenericResultCard";
+import ErrorMessage from "@/components/style/messages/ErrorMessage";
 import { GenericSearchType } from "@/lib/adapters/genericSearchCallback";
 import { generateFakeResultsRaw } from "@/lib/search/search_utils";
 import { sleep } from "@/lib/utils";
 
 export default async function RenderedCardObject({ object_id, objectType, size }: { object_id: string, objectType: GenericSearchType, size?: CardSize }) {
-  await sleep(500);
-  if (!size) {
-    size = CardSize.Large as CardSize;
-  }
-  switch (objectType) {
-    case GenericSearchType.Organization:
-      return <Card size={size} data={generateFakeResultsRaw(1)[0]} />
-    case GenericSearchType.Docket:
-      return <Card size={size} data={generateFakeResultsRaw(1)[0]} />
-    case GenericSearchType.Filling:
-      return <Card size={size} data={generateFakeResultsRaw(1)[0]} />
-    case GenericSearchType.Dummy:
-      return <Card size={size} data={generateFakeResultsRaw(1)[0]} />
+  try {
+    await sleep(500);
+    if (!size) {
+      size = CardSize.Large as CardSize;
+    }
+    switch (objectType) {
+      case GenericSearchType.Organization:
+        return <Card size={size} data={generateFakeResultsRaw(1)[0]} />
+      case GenericSearchType.Docket:
+        return <Card size={size} data={generateFakeResultsRaw(1)[0]} />
+      case GenericSearchType.Filling:
+        return <Card size={size} data={generateFakeResultsRaw(1)[0]} />
+      case GenericSearchType.Dummy:
+        return <Card size={size} data={generateFakeResultsRaw(1)[0]} />
+    }
+  } catch (err) {
+    console.log(err)
+    return <ErrorMessage message="Could not get object info" error={err} />
   }
 }
