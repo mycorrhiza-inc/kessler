43b858b22831de62bc898e7a49e9817c1f254850
Nicole Venner <nvenner@protonmail.ch>
2025-06-27 19:26:54 -0600

    super easy card get

--- backend/internal/objects/files/handler/card_handler.go
+++ backend/internal/objects/files/handler/card_handler.go
@@ -1,12 +1,50 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
+	"kessler/internal/dbstore"
+	"kessler/internal/objects/files"
+	"kessler/internal/search"
+	"kessler/pkg/logger"
+	"net/http"
 	"time"
 
 	"github.com/google/uuid"
-	"kessler/internal/objects/files"
-	"kessler/internal/search"
+	"github.com/gorilla/mux"
+	"go.uber.org/zap"
 )
+
+func (h *FileHandler) FileCardGet(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	ctx, span := tracer.Start(ctx, "files:FileSemiCompleteGet")
+	defer span.End()
+	log := logger.FromContext(ctx)
+
+	params := mux.Vars(r)
+	fileID := params["uuid"]
+	parsedUUID, err := uuid.Parse(fileID)
+	if err != nil {
+		errorstring := fmt.Sprintf("Error parsing file %v: %v", fileID, err)
+		log.Info(errorstring)
+		http.Error(w, errorstring, http.StatusBadRequest)
+		return
+	}
+
+	q := dbstore.New(h.db)
+	file, err := h.SemiCompleteFileGetFromUUID(ctx, q, parsedUUID)
+	if err != nil {
+		log.Info("encountered error getting file from uuid", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// All identical to the card info so far
+	card := FileSchemaToDocCard(file)
+
+	response, _ := json.Marshal(card)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
 
 // FileSchemaToDocCard converts a CompleteFileSchema to a search.DocumentCardData for displaying document cards.
 // This is a basic implementation and can be customized further.
@@ -50,3 +88,4 @@ func FileSchemaToDocCard(fileSchema files.CompleteFileSchema) search.DocumentCar
 		Conversation:   docConv,
 	}
 }
+
