1d2cef2835d69d8fc9b92d34d9a6df03a25b8232
Nicole Venner <nvenner@protonmail.ch>
2025-06-27 19:03:52 -0600

    make a fucking card handler

--- backend/internal/objects/files/handler/card_handler.go
+++ backend/internal/objects/files/handler/card_handler.go
@@ -1,41 +1,52 @@
 package handler
 
 import (
+	"time"
+
+	"github.com/google/uuid"
 	"kessler/internal/objects/files"
 	"kessler/internal/search"
 )
 
-// Example document card data
-//
-//	type DocumentAuthor struct {
-//		AuthorName      string    `json:"author_name"`
-//		IsPerson        bool      `json:"is_person"`
-//		IsPrimaryAuthor bool      `json:"is_primary_author"`
-//		AuthorID        uuid.UUID `json:"author_id"`
-//	}
-//
-//	type DocumentConversation struct {
-//		ConvoName   string    `json:"convo_name"`
-//		ConvoNumber string    `json:"convo_number"`
-//		ConvoID     uuid.UUID `json:"convo_id"`
-//	}
-//
-//	type DocumentCardData struct {
-//		Name           string               `json:"name"`
-//		Description    string               `json:"description"`
-//		Timestamp      time.Time            `json:"timestamp"`
-//		ExtraInfo      string               `json:"extraInfo,omitempty"`
-//		Index          int                  `json:"index"`
-//		Type           string               `json:"type"`
-//		ObjectUUID     uuid.UUID            `json:"object_uuid"`
-//		AttachmentUUID uuid.UUID            `json:"attachment_uuid"`
-//		FragmentID     string               `json:"fragment_id"`
-//		Authors        []DocumentAuthor     `json:"authors"`
-//		Conversation   DocumentConversation `json:"conversation"`
-//	}
-//
-// Here is the location where files.CompleteFileSchema) search.DocumentCardData {
-// /home/nicole/Documents/mycorrhizae/kessler/backend/internal/objects/files/schema.go
-// TODO: Impolement this function DO NOT OVERTHINK EVERYTHING JUST THROW OUT AN IMPLEMENTATION I CAN FULLY IMPROVE
+// FileSchemaToDocCard converts a CompleteFileSchema to a search.DocumentCardData for displaying document cards.
+// This is a basic implementation and can be customized further.
 func FileSchemaToDocCard(fileSchema files.CompleteFileSchema) search.DocumentCardData {
+	// Map authors
+	docAuthors := make([]search.DocumentAuthor, len(fileSchema.Authors))
+	for i, a := range fileSchema.Authors {
+		docAuthors[i] = search.DocumentAuthor{
+			AuthorName:      a.AuthorName,
+			IsPerson:        a.IsPerson,
+			IsPrimaryAuthor: a.IsPrimaryAuthor,
+			AuthorID:        a.AuthorID,
+		}
+	}
+
+	// Map conversation
+	docConv := search.DocumentConversation{
+		ConvoName:   fileSchema.Conversation.Name,
+		ConvoNumber: fileSchema.Conversation.DocketGovID,
+		ConvoID:     fileSchema.Conversation.ID,
+	}
+
+	// Select first attachment if available
+	var attachID uuid.UUID
+	if len(fileSchema.Attachments) > 0 {
+		attachID = fileSchema.Attachments[0].ID
+	}
+
+	// Build the document card data
+	return search.DocumentCardData{
+		Name:           fileSchema.Name,
+		Description:    fileSchema.Extra.Summary,
+		Timestamp:      time.Time(fileSchema.DatePublished),
+		ExtraInfo:      fileSchema.Extra.ShortSummary,
+		Index:          0,
+		Type:           "document",
+		ObjectUUID:     fileSchema.ID,
+		AttachmentUUID: attachID,
+		FragmentID:     "",
+		Authors:        docAuthors,
+		Conversation:   docConv,
+	}
 }
