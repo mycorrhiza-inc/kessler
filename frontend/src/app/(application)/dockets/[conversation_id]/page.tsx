c383f3e22a3b9d26d905a8972100b054ba35b150
Nicole Venner <nvenner@protonmail.ch>
2025-06-29 07:09:29 -0600

    implement individual pages into router

--- frontend/src/app/(application)/dockets/[conversation_id]/page.tsx
+++ frontend/src/app/(application)/dockets/[conversation_id]/page.tsx
@@ -1,13 +1,8 @@
 import { generateTypeUrlParams, } from "@/lib/types/url_params";
-import { ObjectQueryType } from "@/lib/types/SearchTypes";
-import AllInOneServerSearch from "@/components/stateful/SearchBar/AllInOneServerSearch";
 import LoadingSpinner from "@/components/style/misc/LoadingSpinner";
 import { Suspense } from "react";
 import DefaultContainer from "@/components/stateful/PageContainer/DefaultContainer";
-import ServerSearchResults from "@/components/stateful/ServerResults/ServerResults";
-import { GenericSearchType } from "@/lib/adapters/genericSearchCallback";
-import RenderedCardObject, { fetchCardData } from "@/components/stateful/RenderedObjectCards/RednderedObjectCard";
-import { CardSize } from "@/components/style/cards/SizedCards";
+import ConvoPage from "@/components/stateful/ObjectPages/ConvoPage";
 
 export default async function Page({
   params,
@@ -19,23 +14,15 @@ export default async function Page({
 
   const urlParams = generateTypeUrlParams(await searchParams);
   const convo_id = (await params).conversation_id;
-  try {
-    const card_data = fetchCardData(convo_id, GenericSearchType.Docket)
+  if (convo_id == undefined) {
+    throw new Error("Undefined convo id")
   }
 
   return (
     <DefaultContainer>
-      <Suspense fallback={<LoadingSpinner loadingText="Loading Organization Data" />}>
-        <RenderedCardObject objectType={GenericSearchType.Docket} object_id={convo_id} size={CardSize.Large} />
+      <Suspense fallback={<LoadingSpinner loadingText="Loading Docket Data" />}>
+        <ConvoPage convo_id={convo_id} urlParams={urlParams} />
       </Suspense>
-      <AllInOneServerSearch
-        aboveSearchElement={<h1 className="text-2xl font-bold mb-4">Search [org-name]'s Filings</h1>
-        }
-        urlParams={urlParams}
-
-        inherentRouteFilters={{ "conversation_id": convo_id }}
-        baseUrl={`/dockets/${convo_id}`}
-      />
     </DefaultContainer>
   );
 }
--- frontend/src/app/(application)/filing/[file_id]/page.tsx
+++ frontend/src/app/(application)/filing/[file_id]/page.tsx
@@ -1,20 +1,29 @@
 import DocumentServerPage from "@/components/stateful/Document/DocumentServerPage";
+import FilePage from "@/components/stateful/ObjectPages/FilePage";
 import DefaultContainer from "@/components/stateful/PageContainer/DefaultContainer";
+import LoadingSpinner from "@/components/style/misc/LoadingSpinner";
+import { generateTypeUrlParams } from "@/lib/types/url_params";
+import { Suspense } from "react";
 
 // In this project when i visit http://localhost/filing/bf4ebf20-78f8-4f1c-82c8-d8e7c70fa44e
 // it says the filing id is undefined. How can this be happening?
 export default async function Page({
   params,
+  searchParams
 }: {
   params: Promise<{ file_id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) {
+
+  const urlParams = generateTypeUrlParams(await searchParams);
 
   const file_id = (await params).file_id;
   if (file_id === undefined) {
     throw new Error("Undefined file id")
-
   }
   return <DefaultContainer>
-    <DocumentServerPage filing_id={file_id} />
+    <Suspense fallback={<LoadingSpinner loadingText="Loading Filling Data" />}>
+      <FilePage file_id={file_id} urlParams={urlParams} />
+    </Suspense>
   </DefaultContainer>
 }
